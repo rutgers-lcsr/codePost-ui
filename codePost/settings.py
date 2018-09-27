aec3470e586028411919c21290e0384002f3ab9d
James Evans <jaevans@Jamess-MacBook-Pro.local>
2018-09-26 20:33:54 -0400

    remove venv

--- codePost/settings.py
+++ codePost/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'core'
 ]
 
 MIDDLEWARE = [
@@ -118,3 +120,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# DRF settings
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
--- core/models.py
+++ core/models.py
@@ -3,72 +3,74 @@ from django.contrib.auth.models import User
 
 ############# User Section ####################################################
 
-class Profile(models.Model):
-	user = models.OneToOneField(User, on_delete=models.CASCADE)
-	org = models.ForeignKey('Organization')
-
-class StudentUser(models.Model):
-	user = models.OneToOneField(User)
-
-class GraderUser(models.Model):
-	user = models.OneToOneField(User)
-
-class AdminUser(models.Model):
-	user = models.OneToOneField(User)
-
 class Organization(models.Model):
 	name = models.CharField(max_length=64)
 	shortname = models.CharField(max_length=12)
 
+class Profile(models.Model):
+	user = models.OneToOneField(User, on_delete=models.CASCADE)
+	org = models.ForeignKey(Organization, on_delete=models.CASCADE)
+
+class StudentUser(models.Model):
+	user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+class GraderUser(models.Model):
+	user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+class AdminUser(models.Model):
+	user = models.OneToOneField(User, on_delete=models.CASCADE)
+
 ###############################################################################
+
 
 ############# Course Infrastructure Section ####################################
 
 class Course(models.Model):
 	name = models.CharField(max_length=32)
-	org = models.ForeignKey(Organization)
+	org = models.ForeignKey(Organization, on_delete=models.CASCADE)
 	students = models.ManyToManyField(StudentUser)
 	graders = models.ManyToManyField(GraderUser)
 	admins = models.ManyToManyField(AdminUser)
 
 class Section(models.Model):
-	course = models.ForeignKey(Course)
+	course = models.ForeignKey(Course, on_delete=models.CASCADE)
 	students = models.ManyToManyField(StudentUser)
 
 class Assignment(models.Model):
 	name = models.CharField(max_length=32)
-	course = models.ForeignKey(Course)
+	course = models.ForeignKey(Course, on_delete=models.CASCADE)
 	isReleased = models.BooleanField(default=False)
 
 class RubricCategory(models.Model):
-	assignment = models.ForeignKey(Assignment)
+	assignment = models.ForeignKey(Assignment, on_delete=models.CASCADE)
 	name = models.CharField(max_length=32)
 	pointLimit = models.IntegerField()
 
 class RubricComment(models.Model):
 	text = models.TextField()
 	deduction = models.IntegerField()
-	category = models.ForeignKey(RubricCategory)
+	category = models.ForeignKey(RubricCategory, on_delete=models.CASCADE)
 
 ###############################################################################
+
 
 ############# Submissions Section #############################################
 
 class Submission(models.Model):
-	assignment = models.ForeignKey(Assignment)
+	assignment = models.ForeignKey(Assignment, on_delete=models.CASCADE)
 	students = models.ManyToManyField(StudentUser)
-	grader = models.ForeignKey(GraderUser)
+	grader = models.ForeignKey(GraderUser, null=True, on_delete=models.SET_NULL)
 	isFinalized = models.BooleanField()
 
 class File(models.Model):
 	name = models.CharField(max_length=32)
 	code = models.TextField()
-	submission = models.ForeignKey(Submission)
+	submission = models.ForeignKey(Submission, on_delete=models.CASCADE)
 	extension = models.CharField(max_length=8, null=True, blank=True)
 
 class Comment(models.Model):
 	text = models.TextField()
 	deduction = models.IntegerField()
-	rubricComment = models.ForeignKey(RubricComment)
+	rubricComment = models.ForeignKey(RubricComment, null=True, on_delete=models.SET_NULL)
 
 #############################################################################$$
