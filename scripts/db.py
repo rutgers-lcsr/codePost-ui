90a7212b53946da2e663212d94567bc59caa9120
Richard Freling <r@intangiblelabs.co>
2018-10-23 09:55:20 -0400

    enrich restartdb a bit

--- scripts/db.py
+++ scripts/db.py
@@ -22,9 +22,21 @@ vinay.save()
 princeton = Organization.objects.create(name="Princeton University", shortname="Princeton")
 cos126 = CourseParent.objects.create(name='COS126', org=princeton)
 cos126s2019 = Course.objects.create(parent=cos126, period="S2019")
+cos126f2019 = Course.objects.create(parent=cos126, period="F2019")
+
+cos226 = CourseParent.objects.create(name='COS226', org=princeton)
+cos226s2020 = Course.objects.create(parent=cos226, period="S2020")
 
 hello = AssignmentParent.objects.create(name="Hello World", org=princeton)
 hellos2019 = Assignment.objects.create(parent=hello, course=cos126s2019, points=20)
+hellof2019 = Assignment.objects.create(parent=hello, course=cos126f2019, points=20)
+
+nbody = AssignmentParent.objects.create(name="NBody", org=princeton)
+nbodys2019 = Assignment.objects.create(parent=nbody, course=cos126s2019, points=20)
+nbodyf2019 = Assignment.objects.create(parent=nbody, course=cos126f2019, points=20)
+
+percolation = AssignmentParent.objects.create(name="Percolation", org=princeton)
+percolations2020 = Assignment.objects.create(parent=percolation, course=cos226s2020, points=20)
 
 sub = Submission.objects.create(assignment=hellos2019)
 sub.students.add(james.profile.student)
@@ -33,3 +45,7 @@ sub.save()
 
 f1 = File.objects.create(name="hello.java", code="System.out.println('hello world!')", submission=sub, extension='java')
 comment = Comment.objects.create(text="good job!", pointDelta=0, author=richard.profile.grader, file=f1, startChar=1, endChar=3)
+
+richard.profile.student.courses.set(Course.objects.all())
+richard.set_password('pass')
+richard.save()
