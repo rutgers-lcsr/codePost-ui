99674445fcfc5b78d3dcfafda008c8ce6291b6df
jamesaevans <james.alb.evans@gmail.com>
2018-11-06 16:01:29 -0500

    delete boilerplate react code in Home.tsx

--- src/Home.tsx
+++ src/Home.tsx
@@ -3,19 +3,10 @@ import { Link } from 'react-router-dom';
 import { GRADER, STUDENT } from './routes';
 import './styles/App.scss';
 
-import logo from './logo.svg';
-
 class Home extends React.Component {
   public render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
         <p className="App-intro">
           <Link to={STUDENT}>
             Link to Student Page
