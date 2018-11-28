82364d8980cec85b3aba4b70c1c374a64142378f
jamesaevans <james.alb.evans@gmail.com>
2018-11-27 17:18:11 -0800

    remove old send_mail function, which has been replaced by a built-in Django form

--- core/utils.py
+++ core/utils.py
@@ -19,23 +19,6 @@ def my_jwt_response_handler(token, user=None, request=None):
         'user': UserSerializer(user, context={'request': request}).data
     }
 
-def send_mail(subject_template_name, email_template_name,
-              context, from_email, to_email, html_email_template_name=None):
-    """
-    Send a django.core.mail.EmailMultiAlternatives to `to_email`.
-    """
-    subject = loader.render_to_string(subject_template_name, context)
-    # Email subject *must not* contain newlines
-    subject = ''.join(subject.splitlines())
-    body = loader.render_to_string(email_template_name, context)
-
-    email_message = EmailMultiAlternatives(subject, body, from_email, [to_email])
-    if html_email_template_name is not None:
-        html_email = loader.render_to_string(html_email_template_name, context)
-        email_message.attach_alternative(html_email, 'text/html')
-
-    email_message.send()
-
 def emailUser(self, request, subject_template_name, email_template_name):
   form = PasswordResetForm(request.POST)
 
