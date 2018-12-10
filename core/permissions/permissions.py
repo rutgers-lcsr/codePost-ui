c19f2901781a5bf415c69466873195391ae1e97b
jamesaevans <james.alb.evans@gmail.com>
2018-12-10 10:27:54 -0800

    fix indentation

--- core/permissions/permissions.py
+++ core/permissions/permissions.py
@@ -1,7 +1,9 @@
 from rest_framework import permissions
+
+from core.permissions.template import TemplatePermission
 from core.permissions.helpers import isAuthenticated
-from core.permissions.helpers import isStudent, isGrader, isCourseAdmin, isCourseMember
-from core.permissions.helpers import isStudentOfSub, isStaffOfSub
+from core.permissions.helpers import isStudent, isGrader, isCourseAdmin, isCourseMember, isCourseStaff
+from core.permissions.helpers import isSectionLeader, isStudentOfSub, isStaffOfSub
 from core.permissions.helpers import isOrganizationMember
 
 # Notes
@@ -9,141 +11,141 @@ from core.permissions.helpers import isOrganizationMember
 
 ############# User Section ####################################################
 
-class OrganizationPermissions(permissions.BasePermission):
-    def has_object_permission(self, request, view, obj):
-      user = request.user
+class OrganizationPermissions(TemplatePermission):
+  def has_object_permission(self, request, view, obj):
+    user = request.user
 
-      if request.method == "POST":
-        return user.is_superuser
-      if request.method == "DELETE":
-        # Since deleting an organization can have catatrsophic cascase effects,
-        # we should only allow deletion of an organization object from the terminal.
-        # Maybe we can protect it with a confirm pattern.
-        return False
-      if request.method == "PATCH":
-        return user.is_superuser
-      if request.method == "GET":
-        return user.is_superuser
+    if request.method == "POST":
+      return user.is_superuser
+    if request.method == "DELETE":
+      # Since deleting an organization can have catastrophic cascade effects,
+      # we should only allow deletion of an organization object from the terminal.
+      # Maybe we can protect it with a confirm pattern.
+      return False
+    if request.method == "PATCH":
+      return user.is_superuser
+    if request.method == "GET":
+      return user.is_superuser
 
-class UserPermissions(permissions.BasePermission):
-    def has_object_permission(self, request, view, obj):
-      user = request.user
+class UserPermissions(TemplatePermission):
+  def has_object_permission(self, request, view, obj):
+    user = request.user
 
-      if request.method == "POST":
-        return user.is_superuser
-      if request.method == "DELETE":
-        return user.is_superuser
-      if request.method == "PATCH":
-        return user.is_superuser
-      if request.method == "GET":
-        return user.is_superuser or user == obj
+    if request.method == "POST":
+      return user.is_superuser
+    if request.method == "DELETE":
+      return user.is_superuser
+    if request.method == "PATCH":
+      return user.is_superuser
+    if request.method == "GET":
+      return user.is_superuser or user == obj
 
-class CoursePermissions(permissions.BasePermission):
-    def has_object_permission(self, request, view, obj):
-      user = request.user
-      course = obj
+class CoursePermissions(TemplatePermission):
+  def has_object_permission(self, request, view, obj):
+    user = request.user
+    course = obj
 
-      if request.method == "POST":
-        return isCourseAdmin(user, course)
-      if request.method == "DELETE":
-        return isCourseAdmin(user, course)
-      if request.method == "PATCH":
-        return isCourseAdmin(user, course)
-      if request.method == "GET":
-        return isOrganizationMember(user, course.organization)
+    if request.method == "POST":
+      return isCourseAdmin(user, course)
+    if request.method == "DELETE":
+      return isCourseAdmin(user, course)
+    if request.method == "PATCH":
+      return isCourseAdmin(user, course)
+    if request.method == "GET":
+      return isOrganizationMember(user, course.organization)
 
 ################################################################################
 
 ############# Course Infrastructure Section ####################################
 
-class SectionPermissions(permissions.BasePermission):
-    def has_object_permission(self, request, view, obj):
-      user = request.user
-      course = obj.course
+class SectionPermissions(TemplatePermission):
+  def has_object_permission(self, request, view, obj):
+    user = request.user
+    course = obj.course
 
-      if request.method == "POST":
-        return isCourseAdmin(user, course)
-      if request.method == "DELETE":
-        return isCourseAdmin(user, course)
-      if request.method == "PATCH":
-        return isCourseAdmin(user, course)
-      if request.method == "GET":
-        return isCourseAdmin(user, course)
+    if request.method == "POST":
+      return isCourseAdmin(user, course)
+    if request.method == "DELETE":
+      return isCourseAdmin(user, course)
+    if request.method == "PATCH":
+      return isCourseAdmin(user, course)
+    if request.method == "GET":
+      return isSectionLeader(user, obj)
 
-class AssignmentPermissions(permissions.BasePermission):
-    def has_object_permission(self, request, view, obj):
-      user = request.user
-      course = obj.course
+class AssignmentPermissions(TemplatePermission):
+  def has_object_permission(self, request, view, obj):
+    user = request.user
+    course = obj.course
 
-      if request.method == "POST":
-        return isCourseAdmin(user, course)
-      if request.method == "DELETE":
-        return isCourseAdmin(user, course)
-      if request.method == "PATCH":
-        return isCourseAdmin(user, course)
-      if request.method == "GET":
-        return isCourseMember(user, course)
+    if request.method == "POST":
+      return isCourseAdmin(user, course)
+    if request.method == "DELETE":
+      return isCourseAdmin(user, course)
+    if request.method == "PATCH":
+      return isCourseAdmin(user, course)
+    if request.method == "GET":
+      return isCourseMember(user, course)
 
-class RubricCategoryPermissions(permissions.BasePermission):
-    def has_object_permission(self, request, view, obj):
-      user = request.user
-      course = obj.assignment.course
+class RubricCategoryPermissions(TemplatePermission):
+  def has_object_permission(self, request, view, obj):
+    user = request.user
+    course = obj.assignment.course
 
-      if request.method == "POST":
-        return isCourseAdmin(user, course)
-      if request.method == "DELETE":
-        return isCourseAdmin(user, course)
-      if request.method == "PATCH":
-        return isCourseAdmin(user, course)
-      if request.method == "GET":
-        return isCourseAdmin(user, course)
+    if request.method == "POST":
+      return isCourseAdmin(user, course)
+    if request.method == "DELETE":
+      return isCourseAdmin(user, course)
+    if request.method == "PATCH":
+      return isCourseAdmin(user, course)
+    if request.method == "GET":
+      return isCourseStaff(user, course)
 
-class RubricCommentPermissions(permissions.BasePermission):
-    def has_object_permission(self, request, view, obj):
-      user = request.user
-      course = obj.category.assignment.course
+class RubricCommentPermissions(TemplatePermission):
+  def has_object_permission(self, request, view, obj):
+    user = request.user
+    course = obj.category.assignment.course
 
-      if request.method == "POST":
-        return isCourseAdmin(user, course)
-      if request.method == "DELETE":
-        return isCourseAdmin(user, course)
-      if request.method == "PATCH":
-        return isCourseAdmin(user, course)
-      if request.method == "GET":
-        return isCourseAdmin(user, course)
+    if request.method == "POST":
+      return isCourseAdmin(user, course)
+    if request.method == "DELETE":
+      return isCourseAdmin(user, course)
+    if request.method == "PATCH":
+      return isCourseAdmin(user, course)
+    if request.method == "GET":
+      return isCourseStaff(user, course)
 
 ###############################################################################
 
 ############# Submissions Section #############################################
 
-class SubmissionPermissions(permissions.BasePermission):
-    def has_object_permission(self, request, view, obj):
-      user = request.user
-      course = obj.assignment.course
+class SubmissionPermissions(TemplatePermission):
+  def has_object_permission(self, request, view, obj):
+    user = request.user
+    course = obj.assignment.course
 
-      if request.method == "POST":
-        return isCourseAdmin(user, course)
-      if request.method == "DELETE":
-        return isCourseAdmin(user, course)
-      if request.method == "PATCH":
-        return isCourseAdmin(user, course)
-      if request.method == "GET":
-        return isStaffOfSub(user, obj) or isStudentOfSub(user, obj)
+    if request.method == "POST":
+      return isCourseAdmin(user, course)
+    if request.method == "DELETE":
+      return isCourseAdmin(user, course)
+    if request.method == "PATCH":
+      return isCourseAdmin(user, course)
+    if request.method == "GET":
+      return isStaffOfSub(user, obj) or isStudentOfSub(user, obj)
 
-class FilePermissions(permissions.BasePermission):
-    def has_object_permission(self, request, view, obj):
-      return SubmissionPermissions.has_object_permission(self, request, view, obj.submission)
+class FilePermissions(TemplatePermission):
+  def has_object_permission(self, request, view, obj):
+    return SubmissionPermissions.has_object_permission(self, request, view, obj.submission)
 
-class CommentPermissions(permissions.BasePermission):
-    def has_object_permission(self, request, view, obj):
-      user = request.user
-      submission = obj.file.submission
+class CommentPermissions(TemplatePermission):
+  def has_object_permission(self, request, view, obj):
+    user = request.user
+    submission = obj.file.submission
 
-      if request.method == "POST":
-        return isStaffOfSub(user, submission)
-      if request.method == "DELETE":
-        return isStaffOfSub(user, submission)
-      if request.method == "PATCH":
-        return isStaffOfSub(user, submission)
-      if request.method == "GET":
-        return SubmissionPermissions.has_object_permission(self, request, view, submission)
+    if request.method == "POST":
+      return isStaffOfSub(user, submission)
+    if request.method == "DELETE":
+      return isStaffOfSub(user, submission)
+    if request.method == "PATCH":
+      return isStaffOfSub(user, submission)
+    if request.method == "GET":
+      return SubmissionPermissions.has_object_permission(self, request, view, submission)
