3e195f8f1a89d1a28d9dde0b8c397228950dc146
jamesaevans <james.alb.evans@gmail.com>
2018-12-10 20:22:03 -0800

    Alter Organization serializer to automatically set organization to post-ing user's organization

--- core/serializers/course.py
+++ core/serializers/course.py
@@ -7,14 +7,21 @@ class CourseSerializer(ModelSerializerWithPOSTCheck):
   class Meta:
     model = Course
     fields = ('id', 'name', 'period', 'organization', 'assignments', 'sections')
-    read_only_fields = ('assignments',)
-    POST_permissions_fields = ('organization',)
+    read_only_fields = ('assignments', 'organization')
+    POST_permissions_fields = ()
 
   def create(self, validated_data):
-    course = self.tempCreate()
-    # make requesting user a courseAdmin here
-    course.save()
-    return course
+    courseAdmin = self.context['request'].user
+
+    # Organization is required, so have to add before creating object (which
+    # includes saving the object to database)
+    validated_data['organization'] = courseAdmin.profile.organization
+    obj = super().create(validated_data)
+
+    # Force organization equal to posting user's organization
+    obj.courseAdmins.add(courseAdmin)
+    obj.save()
+    return obj
 
 class CourseRosterSerializer(ModelSerializerWithPOSTCheck):
   students = serializers.SlugRelatedField(many=True, slug_field='email', queryset=User.objects.all())
