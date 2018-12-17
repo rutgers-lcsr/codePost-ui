18ba212f905b24ab4939bdac8beb347694488051
jamesaevans <james.alb.evans@gmail.com>
2018-12-17 00:42:49 -0500

    change reference from user.username (which is unused in codePost) to user.email

--- core/models.py
+++ core/models.py
@@ -25,7 +25,7 @@ class Profile(models.Model):
   organization = models.ForeignKey(Organization, on_delete=models.CASCADE, blank=True, null=True)
 
   def __str__(self):
-    return self.user.username
+    return self.user.email
 
 class Course(models.Model):
   name = models.CharField(max_length=16)
