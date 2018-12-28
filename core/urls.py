1908e27b0998f3ed20916d87f49db7c414974526
jamesaevans <james.alb.evans@gmail.com>
2018-12-27 16:18:31 -1000

    remove unused view handler

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path, re_path, include
 from django.views.generic import TemplateView
-from core.views.auth import current_user, UserList
+from core.views.auth import current_user
 
 urlpatterns = [
   path('current_user/', current_user),
-  path('users/', UserList.as_view()),
   re_path('.*', TemplateView.as_view(template_name='index.html')),
 ]
