1607a1ed5970f0e3f0a98c04ad01065c4ed5b919
jamesaevans <james.alb.evans@gmail.com>
2018-12-27 16:17:39 -1000

    merge userserializers

--- core/serializers/user.py
+++ core/serializers/user.py
@@ -1,45 +1,56 @@
 from rest_framework import serializers
+from core.serializers.template import ModelSerializerWithPOSTCheck
 from core.serializers.course import CourseSerializer
 from rest_framework_jwt.settings import api_settings
 from core.models import User, Organization, Profile
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-    organization = serializers.PrimaryKeyRelatedField(source="profile.organization", queryset=Organization.objects.all())
 
-    class Meta:
-        model = User
-        fields = ('email', 'id', 'organization')
+# Helpful source: https://medium.com/@dakota.lillie/django-react-jwt-authentication-5015ee00ef9a
+class UserSerializer(ModelSerializerWithPOSTCheck):
+  organization = serializers.PrimaryKeyRelatedField(source="profile.organization", queryset=Organization.objects.all())
+  # token = serializers.SerializerMethodField()
+  password = serializers.CharField(write_only=True)
 
-class UserWithProfileSerializer(serializers.HyperlinkedModelSerializer):
-    studentCourses = CourseSerializer(many=True, source="student_courses")
-    graderCourses = CourseSerializer(many=True, source="grader_courses")
-    courseadminCourses = CourseSerializer(many=True, source="courseAdmin_courses")
+  class Meta:
+      model = User
+      fields = ('id', 'email', 'password', 'organization')
+      POST_permissions_fields = ()
 
-    class Meta:
-        model = User
-        fields = ('email', 'id', 'studentCourses', 'graderCourses', 'courseadminCourses')
+  # def get_token(self, obj):
+  #     jwt_payload_handler = api_settings.JWT_PAYLOAD_HANDLER
+  #     jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER
 
-# Original source: https://medium.com/@dakota.lillie/django-react-jwt-authentication-5015ee00ef9a
-class UserSerializerWithToken(serializers.ModelSerializer):
-    token = serializers.SerializerMethodField()
-    password = serializers.CharField(write_only=True)
+  #     payload = jwt_payload_handler(obj)
+  #     token = jwt_encode_handler(payload)
+  #     return token
 
-    def get_token(self, obj):
-        jwt_payload_handler = api_settings.JWT_PAYLOAD_HANDLER
-        jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER
+  def create(self, validated_data):
 
-        payload = jwt_payload_handler(obj)
-        token = jwt_encode_handler(payload)
-        return token
+    # Extract parameters that can't be used in User constructor
+    profile = validated_data.pop('profile')
+    password = validated_data.pop('password', None)
 
-    def create(self, validated_data):
-        password = validated_data.pop('password', None)
-        instance = self.Meta.model(**validated_data)
-        if password is not None:
-            instance.set_password(password)
-        instance.save()
-        return instance
+    # Create object
+    validated_data['username'] = validated_data['email']
+    obj = super().create(validated_data)
 
-    class Meta:
-        model = User
-        fields = ('token', 'username', 'password')
+    # Set organization on profile
+    obj.profile.organization = profile["organization"]
+
+    # Set password
+    if password is not None:
+      obj.set_password(password)
+
+    obj.save()
+    return obj
+
+class UserWithProfileSerializer(ModelSerializerWithPOSTCheck):
+  studentCourses = CourseSerializer(many=True, source="student_courses")
+  graderCourses = CourseSerializer(many=True, source="grader_courses")
+  courseadminCourses = CourseSerializer(many=True, source="courseAdmin_courses")
+
+  class Meta:
+      model = User
+      fields = ('email', 'id', 'studentCourses', 'graderCourses', 'courseadminCourses')
+
+# Original source:
