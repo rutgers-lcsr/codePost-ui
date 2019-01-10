0833abfffb1687831e606b8a3781d1d253b26bd2
Richard Freling <r@intangiblelabs.co>
2019-01-10 17:04:01 -0500

    Fix bug

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
 
 import Admin from './Admin';
 
