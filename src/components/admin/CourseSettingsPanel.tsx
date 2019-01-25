d889ea651dab8638afe3ccabbfe90f713604279b
jamesaevans <james.alb.evans@gmail.com>
2019-01-25 16:06:26 -0500

    add support for setting timezone by course in admin view as a setting

--- src/components/admin/CourseSettingsPanel.tsx
+++ src/components/admin/CourseSettingsPanel.tsx
@@ -1,27 +1,39 @@
 import * as React from 'react';
 
-import { CourseType } from '../../infrastructure/course';
+import { CoursePatchType, CourseType } from '../../infrastructure/course';
+import { timezones } from './timezones';
 
 interface IProps {
   currentCourse: CourseType;
-  updateSettings: (newCourse: CourseType) => void;
+  updateSettings: (newCourse: CoursePatchType) => void;
 }
 
 interface IState {
   sendReleasedSubmissionsToBack: boolean;
   showStudentsStatistics: boolean;
+  timezone: string;
 }
 
 class CourseSettingsPanel extends React.Component<IProps, IState> {
   public state: Readonly<IState> = {
     sendReleasedSubmissionsToBack: this.props.currentCourse.sendReleasedSubmissionsToBack,
     showStudentsStatistics: this.props.currentCourse.showStudentsStatistics,
+    timezone: this.props.currentCourse.timezone,
   };
 
   public toggleValue = (label: string, event: any) => {
     this.setState((prevstate) => {
       const newState = { ...prevstate };
       newState[label] = !this.state[label];
+      return newState;
+    });
+  };
+
+  public handleChange = (label: string, event: any) => {
+    const value = event.target.value;
+    this.setState((prevstate) => {
+      const newState = { ...prevstate };
+      newState[label] = value;
       return newState;
     });
   };
@@ -33,17 +45,24 @@ class CourseSettingsPanel extends React.Component<IProps, IState> {
       id: currentCourse.id, // codePost convention
       name: currentCourse.name,
       period: currentCourse.period,
-      assignments: [], // ignored by API
-      sections: [], // ignored by API
       sendReleasedSubmissionsToBack: this.state.sendReleasedSubmissionsToBack,
       showStudentsStatistics: this.state.showStudentsStatistics,
+      timezone: this.state.timezone,
     };
 
     this.props.updateSettings(payload);
   };
 
   public render() {
-    const { sendReleasedSubmissionsToBack, showStudentsStatistics } = this.state;
+    const { sendReleasedSubmissionsToBack, showStudentsStatistics, timezone } = this.state;
+    const timezoneOptions = timezones.map((el, i) => {
+      return (
+        <option key={i} value={el}>
+          {el}
+        </option>
+      );
+    });
+
     return (
       <form onSubmit={this.updateSettings}>
         Show statistics to students{' '}
@@ -60,6 +79,10 @@ class CourseSettingsPanel extends React.Component<IProps, IState> {
           checked={sendReleasedSubmissionsToBack}
         />
         <br />
+        Select a timezone{' '}
+        <select value={timezone} onChange={this.handleChange.bind(this.props, 'timezone')}>
+          {timezoneOptions}
+        </select>
         <br />
         <input type="submit" />
       </form>
