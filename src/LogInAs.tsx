6404050172b5c7c12e74c715db173772cf827067
jamesaevans <james.alb.evans@gmail.com>
2019-01-25 13:02:53 -0500

    update LogInAs to use generic get user/email/ route

--- src/LogInAs.tsx
+++ src/LogInAs.tsx
@@ -20,7 +20,7 @@ class LogInAs extends React.Component<IProps, IState> {
   }
 
   public issueRequest = () => {
-    fetch(`${process.env.REACT_APP_API_URL}/users/1/loginAs/`, {
+    fetch(`${process.env.REACT_APP_API_URL}/users/${this.props.match.params.email}/`, {
       headers: {
         Authorization: `JWT ${localStorage.getItem('token')}`,
       },
