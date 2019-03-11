b46085539329e2970a328cd74a946b51cccb8601
Richard Freling <r@intangiblelabs.co>
2019-03-11 17:46:53 +0000

    setup testing infra

---
+++ src/setupTests.js
@@ -0,0 +1,15 @@
+//----------- Configure Enzyme
+
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
+
+//----------- Enable localStorage usage
+
+const localStorageMock = {
+  getItem: jest.fn(),
+  setItem: jest.fn(),
+  clear: jest.fn()
+};
+global.localStorage = localStorageMock;
