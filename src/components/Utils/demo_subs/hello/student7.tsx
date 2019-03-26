d45b6e0d74de720c2e00ef1e1b0305192f143e81
jamesaevans <james.alb.evans@gmail.com>
2019-03-26 19:38:39 +0000

    add data for demo course

---
+++ src/components/Utils/demo_subs/hello/student7.tsx
@@ -0,0 +1,34 @@
+const submission = (domain: string) => {
+  const studentEmail = `student7@${domain}`;
+  const graderEmail = `grader0@${domain}`;
+  const sectionName = 'Section 3';
+  const partnerSection = 'N/A';
+  const partnerEmail = 'none';
+
+  return {
+    students: [studentEmail],
+    isFinalized: true,
+    grader: graderEmail,
+    files: [
+      {
+        name: 'hello.java',
+        ext: 'java',
+        /* tslint:disable */
+        code: `/******************************************************************************\n *  Student: ${studentEmail}\n *  Section: ${sectionName}\n *\n *  Partner: ${partnerEmail}\n *  Partner section: ${partnerSection}\n *\n *  Description:  Prints \'Hello, World\' to the terminal.\n *                By tradition, this is everyone\'s first program.\n *                Brian Kernighan initiated this tradition in 1974.\n *\n ******************************************************************************/\n\n\n\n\npublic class HelloWorld {\n    public static void main(String[] args) {\n        System.out.println("Hello, World");\n\n\n    }\n}`,
+        /* tslint:ensable */
+        comments: [
+          {
+            text: 'nice!',
+            startChar: 1,
+            endChar: 4,
+            startLine: 0,
+            endLine: 0,
+            pointDelta: 1,
+          },
+        ],
+      },
+    ],
+  };
+};
+
+export default submission;
