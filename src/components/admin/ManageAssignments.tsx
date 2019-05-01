45d7f4f9488099260bce1f2a6a3b7beb1f2f2b9e
Richard Freling <r@intangiblelabs.co>
2019-04-30 22:18:58 -0400

    make assignment name clickable in assignments table

--- src/components/admin/ManageAssignments.tsx
+++ src/components/admin/ManageAssignments.tsx
@@ -464,7 +464,13 @@ class ManageAssignments extends React.Component<IManageAssignmentsProps, IManage
 
         return (
           <TableRow key={assignmentID}>
-            <TableColumn key={`${assignmentID}-1`}>{assignment.name}</TableColumn>
+            <TableColumn
+              key={`${assignmentID}-1`}
+              style={{ cursor: 'pointer' }}
+              onClick={this.changeDetailType.bind(this.props, DETAIL_TYPE.Rubric, assignment)}
+            >
+              {assignment.name}
+            </TableColumn>
             <TableColumn key={`${assignmentID}-2`}>
               <SelectionControl
                 id={`${assignmentID}-release-checkbox`}
