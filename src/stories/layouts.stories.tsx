da5fc0dda140521c3319bfc5320ee4099a494565
vayyala <vayyala08@gmail.com>
2019-06-09 17:43:55 -0500

    fixing stories

--- src/stories/layouts.stories.tsx
+++ src/stories/layouts.stories.tsx
@@ -8,6 +8,10 @@ import CPLayoutGrade from '../components/core/CPLayoutGrade';
 const onClick = (e: any) => null;
 
 storiesOf('Layouts', module)
-  .add('Admin', () => <CPLayoutAdmin onClick={onClick} header={null} detail={null} isRubric={false} />)
+  .add('Admin', () => (
+    <CPLayoutAdmin onClick={onClick} selectedPanel={0} header={null} detail={null} isRubric={false} />
+  ))
   .add('Grade', () => <CPLayoutGrade header={null} subheader={null} files={null} rubric={null} content={null} />)
-  .add('Rubric', () => <CPLayoutAdmin onClick={onClick} header={null} detail={null} isRubric={true} />);
+  .add('Rubric', () => (
+    <CPLayoutAdmin onClick={onClick} selectedPanel={0} header={null} detail={null} isRubric={true} />
+  ));
