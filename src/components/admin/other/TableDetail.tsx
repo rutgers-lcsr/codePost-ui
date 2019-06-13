da1f84b42049d069a0d7a3152ca231539ea17ccc
jamesaevans <james.alb.evans@gmail.com>
2019-06-12 19:49:24 -0700

    make search more general: automatically include all text fields in searchable text, highlight plain text and plain number cells, and allow for definition of a render function that can change based on searchText, e.g. for highlighting

--- src/components/admin/other/TableDetail.tsx
+++ src/components/admin/other/TableDetail.tsx
@@ -17,6 +17,10 @@ import CPAdminDetail from '../other/CPAdminDetail';
 
 /**********************************************************************************************************************/
 
+interface ITableDetailColumn extends ColumnProps<any> {
+  renderForSearch?: (searchText: string) => (text: string, record: any, index: number) => React.ReactNode;
+}
+
 interface IProps {
   loadComplete: boolean;
   isEmpty: boolean;
@@ -24,7 +28,7 @@ interface IProps {
   breadcrumbs?: React.ReactNode;
   emptyNode: React.ReactNode;
   actions: React.ReactNode[];
-  columns: Array<ColumnProps<any>>;
+  columns: ITableDetailColumn[];
   data: any[];
   tableProps?: any;
 }
@@ -43,23 +47,46 @@ class TableDetail extends React.Component<IProps, IState> {
     };
   }
 
-  public getColumnSearchProps = (dataIndex: string) => ({
-    defaultSortOrder: this.defaultSortOrder,
-    render: (text: string, record: any, index: number) =>
-      typeof text === 'string' ? (
-        <Highlighter
-          highlightStyle={{ backgroundColor: '#5CBB8B', padding: 0 }}
-          searchWords={[this.state.searchText]}
-          autoEscape
-          textToHighlight={text.toString()}
-        />
-      ) : (
-        text
-      ),
-  });
+  /* 🔥 alert, the code below is fire 🔥
+   * Columns passed into this component may have an optional 'render' key.
+   * If present, the value of this key is a function which takes a string
+   * argument (representing the current searchText) and returns a function
+   * that renders the table row. The searchText can be used by the returned
+   * function to highlight searched phrases in ReactNode components.
+   *
+   * If not present, this function will try to highlight searchText phrases
+   * in the column iff the column's value (the 'text' value) is a string.
+   * Otherwise, we have no way to inject highlighting into the cell, so
+   * just render the cell value as is.
+   */
+  public getColumnSearchProps = (column: ITableDetailColumn): ColumnProps<any> => {
+    let renderFunction;
+    if (column.render !== undefined) {
+      renderFunction = column.render;
+    } else if (column.renderForSearch !== undefined) {
+      renderFunction = column.renderForSearch(this.state.searchText);
+    } else {
+      renderFunction = (text: string, record: any, index: number) =>
+        typeof text === 'string' ? (
+          <Highlighter
+            highlightStyle={{ backgroundColor: '#5CBB8B', padding: 0 }}
+            searchWords={[this.state.searchText]}
+            autoEscape
+            textToHighlight={text.toString()}
+          />
+        ) : (
+          text
+        );
+    }
+
+    return {
+      defaultSortOrder: this.defaultSortOrder,
+      render: renderFunction,
+    };
+  };
 
   public setSearchText = (event: React.ChangeEvent<HTMLInputElement>) => {
-    this.setState({ searchText: event.target.value });
+    this.setState({ searchText: event.target.value.toUpperCase() });
   };
 
   public render() {
@@ -79,32 +106,35 @@ class TableDetail extends React.Component<IProps, IState> {
       if (this.props.isEmpty) {
         content = this.props.emptyNode;
       } else {
-        const columns = [...this.props.columns];
-        let dataIndex: string = '0';
-
-        /* find first column whose property primaryColumn == 'primary' */
-        columns.forEach((column, i) => {
-          if (column.key === 'primary') {
-            switch (typeof column.key) {
-              case 'string':
-                dataIndex = column.dataIndex!; // FIXME: alter type definition to guarantee this field
-                break;
-              case 'number':
-                dataIndex = column.dataIndex!.toString();
-                break;
-              default:
-                dataIndex = '0';
-                break;
-            }
-            columns[i] = { ...columns[i], ...this.getColumnSearchProps(dataIndex) };
-          }
+        const oldColumns = this.props.columns;
+        const newColumns: Array<ColumnProps<any>> = [];
+        oldColumns.forEach((column, i) => {
+          newColumns[i] = { ...oldColumns[i], ...this.getColumnSearchProps(oldColumns[i]) };
         });
 
+        // Cache keys so we can search each field of the row
+        const keys = this.props.data.length > 0 ? Object.keys(this.props.data[0]) : [];
         const data = this.props.data.filter((el: any) => {
           if (this.state.searchText === '') {
             return true;
           } else {
-            return el[dataIndex].includes(this.state.searchText);
+            for (const key of keys) {
+              if (key !== 'key') {
+                switch (typeof el[key]) {
+                  case 'string':
+                    if (el[key].toUpperCase().includes(this.state.searchText)) {
+                      return true;
+                    }
+                    break;
+                  case 'number':
+                    if (el[key].toUpperCase().includes(this.state.searchText)) {
+                      return true;
+                    }
+                    break;
+                }
+              }
+            }
+            return false;
           }
         });
 
@@ -114,7 +144,7 @@ class TableDetail extends React.Component<IProps, IState> {
             <br />
             <br />
             <Table
-              columns={columns}
+              columns={newColumns}
               dataSource={data}
               pagination={{ showSizeChanger: true, pageSizeOptions: ['10', '50', '100'] }}
               {...(this.props.tableProps ? this.props.tableProps : undefined)}
@@ -138,4 +168,4 @@ class TableDetail extends React.Component<IProps, IState> {
   }
 }
 
-export default TableDetail;
+export { TableDetail, ITableDetailColumn };
