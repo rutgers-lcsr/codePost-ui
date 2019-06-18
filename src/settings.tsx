0bdfacd7ff2c45bd97f3d14726b39bb1de71124b
jamesaevans <james.alb.evans@gmail.com>
2019-06-17 20:15:44 -0400

    restyle user settings page

--- src/settings.tsx
+++ src/settings.tsx
@@ -1,12 +1,26 @@
+/**********************************************************************************************************************/
+/* Imports
+/**********************************************************************************************************************/
+
+/* react imports */
 import * as React from 'react';
 
-import { Button, DialogContainer } from 'react-md';
+/* antd imports */
+import { Icon, Input, Modal, Tooltip, Typography } from 'antd';
 
+/* codePost imports */
 import { UserType } from './infrastructure/user';
+
+import PreAuthLayout from './components/pre-auth/PreAuthLayout';
+
+import CPButton from './components/core/CPButton';
+
+/**********************************************************************************************************************/
 
 interface IState {
   errorMessage: string;
   askedToReset: boolean;
+  loading: boolean;
 }
 
 interface IProps {
@@ -22,27 +36,30 @@ class Settings extends React.Component<IProps, IState> {
     this.state = {
       errorMessage: '',
       askedToReset: false,
+      loading: false,
     };
   }
 
   public requestToken = () => {
-    fetch(`${process.env.REACT_APP_API_URL}/users/requestAPIToken/`, {
-      headers: {
-        Authorization: `JWT ${localStorage.getItem('token') || ''}`,
-        'Content-Type': 'application/json',
-      },
-      method: 'POST',
-    })
-      .then((res) => {
-        if (res.ok) {
-          return res.json();
-        }
-        return Promise.reject();
+    this.setState({ loading: true }, () => {
+      fetch(`${process.env.REACT_APP_API_URL}/users/requestAPIToken/`, {
+        headers: {
+          Authorization: `JWT ${localStorage.getItem('token') || ''}`,
+          'Content-Type': 'application/json',
+        },
+        method: 'POST',
       })
-      .then((json) => {
-        this.props.replaceUser(json, false);
-        this.setState({ askedToReset: false });
-      });
+        .then((res) => {
+          if (res.ok) {
+            return res.json();
+          }
+          return Promise.reject();
+        })
+        .then((json) => {
+          this.props.replaceUser(json, false);
+          this.setState({ askedToReset: false, loading: false });
+        });
+    });
   };
 
   public toggleResetStatus = () => {
@@ -52,77 +69,73 @@ class Settings extends React.Component<IProps, IState> {
   public render() {
     const { user } = this.props;
 
-    let settingsContent;
+    let inputComponent;
     if (user.canModifyRosters) {
       if (user.api_token) {
-        settingsContent = (
-          <div className="UserSettings__settingItem-top">
-            <div className="UserSettings__settingItem__content">
-              <div className="UserSettings__settingItem__name">API token</div>
-              <div className="UserSettings__settingItem__description">
-                This token can be used to authenticate yourself with the{' '}
-                <a href="http://docs.codepost.io">codePost API</a>. For more information on how to use this token,
-                please see our API documentation.
-              </div>
-            </div>
-            <div>{user.api_token}</div>
+        inputComponent = (
+          <Input.Password
+            addonBefore="Your API key"
+            value={user.api_token}
+            addonAfter={
+              <Tooltip title="Reset key">
+                <Icon type="redo" onClick={this.toggleResetStatus} />
+              </Tooltip>
+            }
+          />
+        );
+      } else {
+        inputComponent = (
+          <CPButton cpType="primary" block onClick={this.requestToken} loading={this.state.loading}>
+            Create your first API token
+          </CPButton>
+        );
+      }
+    }
+
+    const settingsContent = (
+      <div>
+        <Typography.Title level={4}>User info</Typography.Title>
+        <Input addonBefore="Email" value={user.email} />
+        <br />
+        <br />
+        <Typography.Title level={4}>Organization info</Typography.Title>
+        Member of: <Typography.Text strong>{user.organization}</Typography.Text>
+        <br />
+        <br />
+        {user.canModifyRosters ? (
+          <div>
+            <Typography.Title level={4}>API token</Typography.Title>
             <div>
-              <Button primary={true} raised={true} onClick={this.toggleResetStatus}>
-                Reset
-              </Button>
+              This token can be used to authenticate yourself with the
+              <a href="http://docs.codepost.io"> codePost API</a>. For more information on how to use this token, please
+              see our API documentation.
             </div>
-            <DialogContainer
+            <br />
+            {inputComponent}
+            <Modal
               visible={this.state.askedToReset}
-              id="deleteSubmission-dialog"
-              className="Dialog--deleteSubmission"
               title="Are you sure you want to reset your API token?"
-              modal
+              okText="Reset"
+              onOk={this.requestToken}
+              onCancel={this.toggleResetStatus}
             >
               <div>
                 Your old API key will no longer work. You will need to replace all instances of your old key with your
                 new key.
               </div>
-              <br />
-              <Button onClick={this.toggleResetStatus} primary={true} flat={true}>
-                Cancel
-              </Button>
-              <Button onClick={this.requestToken} primary={false} flat={true}>
-                Reset
-              </Button>
-            </DialogContainer>
+            </Modal>
           </div>
-        );
-      } else {
-        settingsContent = (
-          <div className="UserSettings__settingItem-top">
-            <div className="UserSettings__settingItem__content">
-              <div className="UserSettings__settingItem__name">API token</div>
-              <div className="UserSettings__settingItem__description">
-                This token can be used to authenticate yourself with the <a href="docs.codepost.io">codePost API</a>.
-                For more information on how to use this token, please see our API documentation.
-              </div>
-            </div>
-            <div>Create a token: </div>
-            <div>
-              <button onClick={this.requestToken}>create</button>
-            </div>
-          </div>
-        );
-      }
-    } else {
-      settingsContent = (
-        <div className="UserSettings__settingItem-top">
-          <div> Your Setings Go Here! </div>
-        </div>
-      );
-    }
-    return (
-      <div className="UserSettings">
-        <div className="UserSettings__main-container">
-          <div className="UserSettings__title">User Settings</div>
-          <div className="UserSettings__main-container__items">{settingsContent}</div>
-        </div>
+        ) : null}
       </div>
+    );
+
+    return (
+      <PreAuthLayout isLoggedIn={true}>
+        <div style={{ width: 600, margin: '0 auto' }}>
+          <Typography.Title level={3}>Your settings:</Typography.Title>
+          {settingsContent}
+        </div>
+      </PreAuthLayout>
     );
   }
 }
