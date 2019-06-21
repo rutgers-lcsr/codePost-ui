be6df0779337db219586c280ff37b9a18faaa040
jamesaevans <james.alb.evans@gmail.com>
2019-06-21 09:09:18 -0700

    folder case change

---
+++ src/components/landing/LandingAnimations/API/APIAnimation.tsx
@@ -0,0 +1,137 @@
+import { Icon } from 'antd';
+import React from 'react';
+import { animated, useSpring } from 'react-spring';
+import { AbsoluteCircle, Circle, hexToRGB } from './Utils';
+
+// Definition of the position (on a 500x400 pixel grid), color, icon, and names of the auxillary circles
+const iconSize = 20;
+const circleMap = [
+  { name: 'LMS', icon: 'idcard', toneColor: '#f1c40f', fontSize: iconSize, x: 150, y: 265, top: -20, left: 40 },
+  { name: 'Autograder', icon: 'code', toneColor: '#16a085', fontSize: iconSize, x: 350, y: 265, top: -20, left: -15 },
+  {
+    name: 'Version control',
+    icon: 'edit',
+    toneColor: '#e67e22',
+    fontSize: iconSize,
+    x: 250,
+    y: 80,
+    top: 45,
+    left: 30,
+  },
+  {
+    name: 'Homegrown tools',
+    icon: 'database',
+    toneColor: '#d35400',
+    fontSize: iconSize,
+    x: 250,
+    y: 375,
+    top: -30,
+    left: 30,
+  },
+  { name: 'Registrar', icon: 'bank', toneColor: '#3498db', fontSize: iconSize, x: 105, y: 113, top: 25, left: 40 },
+  {
+    name: 'Plagiarism detection',
+    icon: 'safety-certificate',
+    toneColor: '#9b59b6',
+    fontSize: iconSize,
+    x: 395,
+    y: 113,
+    top: 25,
+    left: -10,
+  },
+];
+
+const iconMap = [
+  { key: 'github', item: <img src={require('./GitHub.png')} style={{ width: 20, zIndex: 30 }} />, top: 300, left: 295 },
+  {
+    key: 'jupyter',
+    item: <img src={require('./Jupyter.png')} style={{ width: 30, zIndex: 30 }} />,
+    top: 350,
+    left: 175,
+  },
+  {
+    key: 'moss',
+    item: <div style={{ fontStyle: 'italic', fontWeight: 500, color: 'green' }}>MOSS</div>,
+    top: 230,
+    left: 45,
+  },
+];
+
+export default function APIAnimation() {
+  // Creating the perpetual spring for the motion of the dashed paths
+  const f: any = async (next: any) => {
+    while (1) await next({ amt: 110 });
+  };
+  const { amt }: any = useSpring({
+    to: f,
+    from: { amt: 10 },
+    config: { duration: 3500 },
+    reset: true,
+  });
+
+  // Definition of other constants (codePost central circle, and icons)
+  const codePostText = (
+    <div>
+      <div style={{ fontSize: 18, fontFamily: 'sans-serif', fontWeight: 300 }}>codePost</div>
+      <div style={{ fontSize: 24, fontFamily: 'sans-serif', fontWeight: 700, color: 'grey', textAlign: 'center' }}>
+        API
+      </div>
+    </div>
+  );
+  const cPCircle = <Circle item={codePostText} radius={40} borderWidth={0} color={'grey'} />;
+
+  // First position logos
+  // Then position center codePost circle
+  // Then position auxillary circles
+  // Then draw the animated paths
+  return (
+    <div style={{ width: 500, height: 400, marginTop: 30, position: 'relative' }}>
+      {iconMap.map((logo) => {
+        return (
+          <div key={logo.key} style={{ position: 'absolute', top: logo.top, left: logo.left }}>
+            {logo.item}
+          </div>
+        );
+      })}
+      <AbsoluteCircle x={250} y={200} radius={40} boxWidth={500} boxHeight={400} item={cPCircle} />
+      {circleMap.map((row) => {
+        const icon = (
+          <div style={{ position: 'relative' }}>
+            <Icon
+              type={row.icon}
+              theme="twoTone"
+              twoToneColor={row.toneColor}
+              style={{ fontSize: row.fontSize, position: 'absolute', top: row.top, left: row.left, zIndex: 10 }}
+            />
+            <div style={{ fontSize: 12, fontWeight: 300, textAlign: 'center' }}>{row.name}</div>
+          </div>
+        );
+        const circle = <Circle item={icon} radius={40} borderWidth={2} color={hexToRGB(row.toneColor, '0.15')} />;
+        return (
+          <AbsoluteCircle key={row.name} x={row.x} y={row.y} radius={40} boxWidth={500} boxHeight={400} item={circle} />
+        );
+      })}
+      <div style={{ position: 'absolute', top: 0 }}>
+        <animated.svg
+          height="420"
+          width="500"
+          strokeDasharray={10}
+          strokeDashoffset={amt.interpolate((x: number) => x)}
+        >
+          <circle
+            cx="250"
+            cy="200"
+            r="100"
+            fill="transparent"
+            stroke="#24be85"
+            stroke-width="3"
+            transform="rotate(-25 250 200)"
+          />
+          <path d="M250 335 Q 50 350 130 145" stroke="grey" fill="transparent" />
+          <path d="M370 145 Q 450 350 250 335" stroke="grey" fill="transparent" />
+          <path d="M130 145 Q 250 -75 370 145" stroke="grey" fill="transparent" />
+        </animated.svg>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/landing/LandingAnimations/Admin/SimpleRubricCategory.tsx
@@ -0,0 +1,102 @@
+import * as React from 'react';
+
+import { Badge, Icon, Input, Table } from 'antd';
+
+import CPButton from '../../../../components/core/CPButton';
+import CPFlex from '../../../../components/core/CPFlex';
+
+import { RubricCategoryType } from '../../../../infrastructure/rubricCategory';
+import { RubricCommentType } from '../../../../infrastructure/rubricComment';
+
+type alignType = 'left' | 'right' | 'center';
+
+interface ICPRubricCategoryProps {
+  rubricCategory: RubricCategoryType;
+  rubricComments: RubricCommentType[];
+
+  // deleteCategory: () =>;
+
+  // addComment: () => ;
+  // deleteComment: () => ;
+}
+const centerAlign: alignType = 'center';
+const commentTableColumns = [
+  {
+    title: 'Comment Text',
+    dataIndex: 'text',
+    key: 'text',
+  },
+  {
+    title: 'Deduction',
+    dataIndex: 'deduction',
+    key: 'deduction',
+    align: centerAlign,
+  },
+  {
+    title: 'Usage Frequency',
+    key: 'linked',
+    dataIndex: 'linked',
+    align: centerAlign,
+    render: (count: number, record: any) => (
+      <Badge count={count} className="cp-badge" style={{ backgroundColor: 'rgba(0,0,0,0.5)' }} />
+    ),
+  },
+  {
+    title: '',
+    dataIndex: 'delete',
+    key: 'delete',
+    align: centerAlign,
+    render: (record: any) => <Icon type="delete" />,
+  },
+];
+
+class SimpleRubricCategory extends React.Component<ICPRubricCategoryProps, {}> {
+  public buildCommentTableData = (rubricComments: RubricCommentType[]) => {
+    return rubricComments.map((rubricComment: RubricCommentType, index: number) => {
+      return {
+        key: index,
+        text: rubricComment.text,
+        deduction: rubricComment.pointDelta,
+        linked: rubricComment.comments.length,
+      };
+    });
+  };
+
+  public render() {
+    const data = this.buildCommentTableData(this.props.rubricComments);
+
+    const content = [
+      <div key="name">
+        <div className="cp-label cp-label--bold" style={{}}>
+          Category Name
+        </div>
+        <Input value={this.props.rubricCategory.name} />
+      </div>,
+      <div key="points">
+        <div className="cp-label cp-label--bold" style={{}}>
+          Category Point Limit
+        </div>
+        <Input value={'3.0'} />
+      </div>,
+    ];
+
+    // Should we have Add New Category within this table?
+    return (
+      <div style={{ marginBottom: 10 }}>
+        <div className="cp-rubric-category__content" style={{ paddingBottom: 60, paddingLeft: 15, paddingRight: 15 }}>
+          <CPFlex left={content} right={[]} gutterSize={60} />
+          <div style={{ height: '40px' }} />
+          <Table columns={commentTableColumns} dataSource={data} pagination={false} />
+          <div className="cp-rubric-category__add-new-comment" style={{ bottom: 10 }}>
+            <CPButton cpType="primary" icon="plus" />
+            <span style={{ marginLeft: '20px' }} className="cp-label cp-label--success cp-label--bold">
+              ADD NEW COMMENT
+            </span>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default SimpleRubricCategory;
