6f6537245c0b57c1a5c54b7257e3c70b41cc8613
Richard Freling <r@intangiblelabs.co>
2019-06-21 14:19:01 -0500

    fix

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -156,7 +156,7 @@ module.exports = {
                   // disable type checker - we will use it in fork plugin
                   transpileOnly: true,
                   getCustomTransformers: () => ({
-                    before: [ tsImportPluginFactory( { style: 'css' } ) ]
+                    before: [ tsImportPluginFactory( { style: true } ) ]
                   }),
                   compilerOptions: {
                     module: 'es2015'
@@ -165,51 +165,6 @@ module.exports = {
                 },
               },
             ],
-          },
-          //------------------- For Ant Theme Override -------------------//
-          // https://ant.design/docs/react/customize-theme
-          {
-            test: /\.less$/,
-            use: [{
-              loader: 'style-loader',
-            }, {
-              loader: 'css-loader', // translates CSS into CommonJS
-            }, {
-              loader: 'less-loader', // compiles Less to CSS
-              options: {
-                modifyVars: themeVars.ant,
-                javascriptEnabled: true,
-              },
-            }],
-          },
-          //------------------- Add SCSS Loaders -------------------//
-          // https://medium.com/@oreofeolurin/configuring-scss-with-react-create-react-app-1f563f862724
-          {
-            test: /\.scss/,
-            use: [{
-              loader: 'style-loader',
-            }, {
-              loader: 'css-loader',
-            }, {
-              loader: 'sass-loader',
-              // ------ Sharing variables between JS and SASS --------//
-              // https://itnext.io/sharing-variables-between-js-and-sass-using-webpack-sass-loader-713f51fa7fa0
-              options: {
-                functions: {
-                  "get($keys)": function(keys) {
-                    keys = keys.getValue().split(".");
-                    let result = themeVars;
-                    let i;
-                    for (i = 0; i < keys.length; i++) {
-                      result = result[keys[i]];
-                    }
-                    result = sassUtils.castToSass(result);
-                    return result;
-                  }
-                }
-              }
-
-            }],
           },
           // The notation here is somewhat confusing.
           // "postcss" loader applies autoprefixer to our CSS.
@@ -270,6 +225,53 @@ module.exports = {
             ),
             // Note: this won't work without `new ExtractTextPlugin()` in `plugins`.
           },
+
+          //------------------- For Ant Theme Override -------------------//
+          // https://ant.design/docs/react/customize-theme
+          {
+            test: /\.less$/,
+            use: [{
+              loader: 'style-loader',
+            }, {
+              loader: 'css-loader', // translates CSS into CommonJS
+            }, {
+              loader: 'less-loader', // compiles Less to CSS
+              options: {
+                modifyVars: themeVars.ant,
+                javascriptEnabled: true,
+              },
+            }],
+          },
+          //------------------- Add SCSS Loaders -------------------//
+          // https://medium.com/@oreofeolurin/configuring-scss-with-react-create-react-app-1f563f862724
+          {
+            test: /\.scss/,
+            use: [{
+              loader: 'style-loader',
+            }, {
+              loader: 'css-loader',
+            }, {
+              loader: 'sass-loader',
+              // ------ Sharing variables between JS and SASS --------//
+              // https://itnext.io/sharing-variables-between-js-and-sass-using-webpack-sass-loader-713f51fa7fa0
+              options: {
+                functions: {
+                  "get($keys)": function(keys) {
+                    keys = keys.getValue().split(".");
+                    let result = themeVars;
+                    let i;
+                    for (i = 0; i < keys.length; i++) {
+                      result = result[keys[i]];
+                    }
+                    result = sassUtils.castToSass(result);
+                    return result;
+                  }
+                }
+              }
+
+            }],
+          },
+
           // "file" loader makes sure assets end up in the `build` folder.
           // When you `import` an asset, you get its filename.
           // This loader doesn't use a "test" so it will catch all modules
