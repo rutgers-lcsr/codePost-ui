8b3c76d3548cb4bb9dead4da7c1e4557a33837ef
jamesaevans <james.alb.evans@gmail.com>
2019-06-23 16:31:54 -0700

    open submissions in new tab instead of new window

--- src/components/admin/other/AdminUtils.tsx
+++ src/components/admin/other/AdminUtils.tsx
@@ -6,7 +6,7 @@
 
 function openSubmission(submissionID: number | string) {
   if (window) {
-    window.open(`/grade/${submissionID}`, 'test', `width=${screen.availWidth * 0.9},height=${screen.availHeight}0.9`);
+    window.open(`/grade/${submissionID}`);
   }
 }
 
