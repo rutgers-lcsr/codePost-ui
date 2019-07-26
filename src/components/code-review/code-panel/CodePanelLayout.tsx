bb17e3e710ab42e3fbc6edfbb646a768e34e0287
Richard <richard@codepost.io>
2019-07-26 14:30:22 -0400

    udno

--- src/components/code-review/code-panel/CodePanelLayout.tsx
+++ src/components/code-review/code-panel/CodePanelLayout.tsx
@@ -147,7 +147,7 @@ class LayoutCodePanel extends React.Component<ICodePanelLayoutProps, {}> {
               <div
                 id="code-panel--comments"
                 className="code-panel--comments"
-                style={{ minWidth: `${this.props.dimensions.commentsWidth} px` }}
+                style={{ minWidth: `${this.props.dimensions.commentsWidth}px` }}
               >
                 {this.props.comments}
               </div>
