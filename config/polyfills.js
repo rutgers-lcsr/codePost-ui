7afb2c467dcff05d37bc8e38c24c6c75464aaff2
Richard <richard@codepost.io>
2019-08-18 22:27:09 -0400

    add babel-polyfill

--- config/polyfills.js
+++ config/polyfills.js
@@ -3,6 +3,7 @@
 require('react-app-polyfill/ie9');
 require('react-app-polyfill/ie11');
 require('react-app-polyfill/stable');
+require('babel-polyfill');
 
 if (typeof Promise === 'undefined') {
   // Rejection tracking prevents a common issue where React gets into an
