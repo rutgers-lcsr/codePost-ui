fc044c9856177cde8298ed15105065919c935f2c
Richard <richard@codepost.io>
2019-08-29 18:55:43 -0700

    add beta tags

--- src/components/pre-auth/WhyUse.tsx
+++ src/components/pre-auth/WhyUse.tsx
@@ -41,7 +41,7 @@ const sections = [
     features: [
       'Make inline comments easily, with markdown',
       <span key="template">
-        Gray out template code &nbsp; <Tag>NEW</Tag>
+        Gray out template code &nbsp; <Tag>BETA</Tag>
       </span>,
       'Annotate IPython / Jupyter notebooks',
       <span key="text">
@@ -49,7 +49,9 @@ const sections = [
         <Typography.Text code>.md</Typography.Text>)
       </span>,
       'Apply rubric items while grading',
-      'Collaboratively create the rubric',
+      <span key="template">
+        Collaboratively create the rubric &nbsp; <Tag>BETA</Tag>
+      </span>,
       'Dark Mode!',
     ],
     spotlights: [
