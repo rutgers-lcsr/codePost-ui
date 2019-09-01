6a6a15b9cfc8fad98dcced099cc4e7f8abba492c
jamesaevans <james.alb.evans@gmail.com>
2019-09-01 13:44:11 -0700

    generic infrastructure for emailing

---
+++ src/components/admin/other/SendEmailModal.tsx
@@ -0,0 +1,123 @@
+/**********************************************************************************************************************/
+/* Imports
+/**********************************************************************************************************************/
+
+/* react imports */
+import * as React from 'react';
+
+/* style imports */
+import { Button, message, Modal } from 'antd';
+
+/* codePost imports */
+
+// type definitions
+import { AssignmentType } from '../../../infrastructure/assignment';
+import { CourseType } from '../../../infrastructure/course';
+
+import CPButton from '../../core/CPButton';
+
+/**********************************************************************************************************************/
+
+interface IProps {
+  buttonText: string;
+  title: string;
+  filterFunction: () => string[];
+  template: string;
+  course: CourseType;
+  assignment?: AssignmentType;
+  me: string;
+  body: React.ReactNode;
+}
+
+interface IState {
+  /* are we in the middle of sending an email? */
+  isSending: boolean;
+
+  modalVisible: boolean;
+}
+
+class SendEmailModal extends React.Component<IProps, IState> {
+  public constructor(props: IProps) {
+    super(props);
+    this.state = {
+      isSending: false,
+      modalVisible: false,
+    };
+  }
+
+  public sendTestEmail = () => {
+    this.sendEmails([this.props.me], false);
+  };
+
+  public sendLiveEmails = () => {
+    this.setState({ isSending: true }, () => {
+      const toEmail = this.props.filterFunction();
+      this.sendEmails(toEmail, true);
+    });
+    this.toggleDialog();
+  };
+
+  public sendEmails = (toSend: string[], livemode: boolean) => {
+    this.setState({ isSending: true }, () => {
+      toSend.forEach((users) => {
+        fetch(`${process.env.REACT_APP_API_URL}/users/${users}/email/`, {
+          body: JSON.stringify({
+            token: localStorage.getItem('token'),
+            template: 'grader_reminder',
+            assignment: this.props.assignment !== undefined ? this.props.assignment.id : undefined,
+            course: this.props.course.id,
+            livemode,
+          }),
+          headers: {
+            Authorization: `JWT ${localStorage.getItem('token') || ''}`,
+            'Content-Type': 'application/json',
+          },
+          method: 'POST',
+        });
+      });
+    });
+
+    this.setState({ isSending: false });
+    message.success('Emails successfully sent.');
+  };
+
+  public toggleDialog = () => {
+    this.setState({
+      modalVisible: !this.state.modalVisible,
+    });
+  };
+
+  public render() {
+    return (
+      <div>
+        <CPButton cpType="secondary" icon="mail" loading={this.state.isSending} onClick={this.toggleDialog}>
+          {this.props.buttonText}
+        </CPButton>
+        <Modal
+          visible={this.state.modalVisible}
+          onCancel={this.toggleDialog}
+          title={this.props.title}
+          width={600}
+          footer={[
+            <Button key="back" onClick={this.toggleDialog}>
+              Cancel
+            </Button>,
+            <Button key="submit" type="primary" disabled={false} onClick={this.sendLiveEmails}>
+              Send
+            </Button>,
+          ]}
+        >
+          {this.props.body}
+          <br />
+          <div>
+            To see what this email will look like, send yourself a test email.
+            <CPButton onClick={this.sendTestEmail} cpType="secondary">
+              Send myself a test email
+            </CPButton>
+          </div>
+        </Modal>
+      </div>
+    );
+  }
+}
+export default SendEmailModal;
---
+++ src/components/admin/roster/other/RosterUtils.tsx
@@ -0,0 +1,27 @@
+import { AssignmentType } from '../../../../infrastructure/assignment';
+import { CourseType } from '../../../../infrastructure/course';
+
+const sendEmailToUser = (
+  user: string,
+  template: string,
+  course: CourseType,
+  livemode: boolean,
+  assignment?: AssignmentType,
+) => {
+  fetch(`${process.env.REACT_APP_API_URL}/users/${user}/email/`, {
+    body: JSON.stringify({
+      token: localStorage.getItem('token'),
+      template: 'grader_reminder',
+      assignment: assignment !== undefined ? assignment.id : undefined,
+      course: course.id,
+      livemode,
+    }),
+    headers: {
+      Authorization: `JWT ${localStorage.getItem('token') || ''}`,
+      'Content-Type': 'application/json',
+    },
+    method: 'POST',
+  });
+};
+
+export { sendEmailToUser };
