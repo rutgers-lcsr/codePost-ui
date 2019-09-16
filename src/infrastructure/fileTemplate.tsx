cbaaa55d64519c2a84373a62441a54d9da3cd29c
Richard <richard@codepost.io>
2019-09-16 10:32:45 -0700

    FileTemplateType

---
+++ src/infrastructure/fileTemplate.tsx
@@ -0,0 +1,40 @@
+import * as t from 'io-ts';
+import { createObject, deleteObject, GenericObject, readObject } from './generics';
+
+const FileTemplateV = t.intersection(
+  [
+    GenericObject,
+    t.type({
+      code: t.string,
+      extension: t.string,
+      name: t.string,
+      assignment: t.number,
+      path: t.union([t.string, t.null]),
+    }),
+    t.partial({}),
+  ],
+  'FileTemplate',
+);
+
+const FileVPost = t.intersection(
+  [
+    GenericObject,
+    t.type({
+      code: t.string,
+      extension: t.string,
+      name: t.string,
+      assignment: t.number,
+      path: t.union([t.string, t.null]),
+    }),
+    t.partial({}),
+  ],
+  'FileTemplatePost',
+);
+
+export type FileTemplateType = t.TypeOf<typeof FileTemplateV>;
+
+export class FileTemplate {
+  public static create = createObject(FileTemplateV, FileTemplateVPost, 'fileTemplates');
+  public static read = readObject(FileTemplateV, 'fileTemplates');
+  public static delete = deleteObject(FileTemplateV, 'fileTemplates');
+}
