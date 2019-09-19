f939a1c6b52e905364cacc897f410051bd7c1d0c
Richard <richard@codepost.io>
2019-09-19 09:33:27 -0700

    update settings capital convention; close settings on save

--- src/components/admin/assignments/assignments/AssignmentSettingsDialog.tsx
+++ src/components/admin/assignments/assignments/AssignmentSettingsDialog.tsx
@@ -52,6 +52,7 @@ class AssignmentSettingsDialog extends React.Component<IProps, {}> {
 
     this.props.onSave(payload).then(() => {
       message.success('Assignment settings updated!');
+      this.props.onCancel();
     });
   };
 
@@ -332,7 +333,7 @@ const CollectionCreateForm: any = Form.create()(
                   })(<Switch />)}
                 </Form.Item>
                 <Form.Item
-                  label="Collaborative Rubric"
+                  label="Collaborative rubric"
                   extra={
                     <div>
                       When enabled, admins and graders will be able to edit the assignment rubric inline in the code
