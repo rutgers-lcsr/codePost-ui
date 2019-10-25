bf300a742baaa34e6909dec381ea3cffa5614680
Richard <richard@codepost.io>
2019-10-25 16:27:50 -0500

    cursorless code!

--- src/components/code-review/code-panel/Code.tsx
+++ src/components/code-review/code-panel/Code.tsx
@@ -19,6 +19,87 @@ interface ICodeProps {
 const Code = (props: ICodeContentCoreProps & ICodeContentEditProps & ICodeProps) => {
   const { consoleTheme } = React.useContext(ConsoleThemeContext);
 
+  const [showCursor, setShowCursor] = React.useState(false);
+  const [[cursorSelectLine, cursorSelectExtent], setCursorSelect] = React.useState([0, 1]);
+
+  React.useEffect(() => {
+    const lines = props.file.code.split('\n');
+
+    const handleKeydown = async (e: any) => {
+      console.log('key', e);
+      if (!showCursor && ['ArrowUp', 'ArrowDown'].includes(e.key)) {
+        e.preventDefault();
+        e.stopPropagation();
+        setShowCursor(true);
+      } else if (e.shiftKey && e.key === 'ArrowUp') {
+        e.preventDefault();
+        e.stopPropagation();
+        console.log('SHIFT UP');
+        if (cursorSelectLine >= 0) {
+          setCursorSelect([cursorSelectLine, Math.max(cursorSelectExtent - 1, 1)]);
+        }
+      } else if (e.shiftKey && e.key === 'ArrowDown') {
+        e.preventDefault();
+        e.stopPropagation();
+        console.log('SHIFT DOWN');
+        if (cursorSelectLine >= 0) {
+          console.log('a', cursorSelectExtent + 1 - cursorSelectLine);
+          console.log('b', lines.length - 1 - cursorSelectLine);
+          setCursorSelect([cursorSelectLine, Math.min(cursorSelectExtent + 1, lines.length - cursorSelectLine)]);
+        }
+      } else if (e.key === 'ArrowUp') {
+        e.preventDefault();
+        e.stopPropagation();
+        setCursorSelect([Math.max(cursorSelectLine - 1, 0), cursorSelectExtent]);
+        console.log('UP');
+      } else if (e.key === 'ArrowDown') {
+        e.preventDefault();
+        e.stopPropagation();
+        console.log('DOWN');
+        setCursorSelect([Math.min(cursorSelectLine + 1, lines.length - cursorSelectExtent), cursorSelectExtent]);
+      }
+
+      if (e.key === 'Enter') {
+        console.log('ENTER:', cursorSelectLine);
+        if (cursorSelectLine >= 0) {
+          e.preventDefault();
+          e.stopPropagation();
+
+          const startLine = cursorSelectLine;
+          const endLine = cursorSelectLine + cursorSelectExtent - 1;
+          const startChar = 0;
+          const endChar = lines[endLine].length;
+          const newComment: CommentType = {
+            id: props.commentCounter,
+            endChar,
+            endLine,
+            file: props.file.id,
+            pointDelta: 0.0,
+            startChar,
+            startLine,
+            text: '',
+            rubricComment: null,
+            author: props.user,
+            feedback: 0,
+          };
+
+          props.addComment(newComment, props.file);
+
+          // FIXME: we can come up with a better solution
+          await wait(5);
+
+          setCursorSelect([cursorSelectLine, 1]);
+          setShowCursor(false);
+          CodePanelHighlighting.brightenHighlight(newComment.id, consoleTheme.highlightActive);
+        }
+      }
+    };
+    document.addEventListener('keydown', handleKeydown);
+    return () => {
+      document.removeEventListener('keydown', handleKeydown);
+    };
+  });
+
   const onMouseUp = async (event: React.MouseEvent) => {
     const selection = window.getSelection();
     // https://developer.mozilla.org/en-US/docs/Web/API/Selection/isCollapsed
@@ -122,10 +203,19 @@ const Code = (props: ICodeContentCoreProps & ICodeContentEditProps & ICodeProps)
     document.addEventListener('mouseup', callback);
   };
 
+  console.log('> ', cursorSelectLine, cursorSelectExtent);
+
   const linesOfCode = (readOnly: boolean, code: string, comments: CommentType[]) => {
     return code.split('\n').map((text: string, i: number) => {
+      const style =
+        showCursor && i >= cursorSelectLine && i < cursorSelectLine + cursorSelectExtent
+          ? {
+              backgroundColor: 'lightblue',
+              opacity: 0.2,
+            }
+          : {};
       return (
-        <div key={i} id={`line-${i}`} onMouseDown={readOnly ? undefined : onMouseDown}>
+        <div key={i} id={`line-${i}`} onMouseDown={readOnly ? undefined : onMouseDown} style={style}>
           {text === ''
             ? ' '
             : CodePanelHighlighting.highlight(
