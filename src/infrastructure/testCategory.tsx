7fe6da9ab37eaecb1628dc8e228b389009d09b8a
vayyala <vayyala08@gmail.com>
2019-11-22 17:59:34 -0500

    remove run endpoint from testcategory

--- src/infrastructure/testCategory.tsx
+++ src/infrastructure/testCategory.tsx
@@ -1,6 +1,5 @@
 import * as t from 'io-ts';
-import { createObject, deleteObject, GenericObject, readObject, updateObject, createObjectDetail } from './generics';
-import { TaskV } from './autograder/runTypes';
+import { createObject, deleteObject, GenericObject, readObject, updateObject } from './generics';
 
 const TestCategoryV = t.intersection(
   [
@@ -9,7 +8,6 @@ const TestCategoryV = t.intersection(
       assignment: t.number,
       name: t.string,
       testCases: t.array(t.number),
-      type: t.string,
     }),
   ],
   'TestCategory',
@@ -21,7 +19,6 @@ const TestCategoryPostV = t.intersection(
     t.type({
       assignment: t.number,
       name: t.string,
-      type: t.string,
     }),
   ],
   'TestCategory',
@@ -37,18 +34,12 @@ const TestCategoryPatchV = t.intersection(
   'TestCategory',
 );
 
-//  Defail Route : Run
-const TestInputs = t.intersection([GenericObject, t.type({ submission: t.union([t.number, t.null]) })]);
-
 export type TestCategoryType = t.TypeOf<typeof TestCategoryV>;
-
 export class TestCategory {
   public static create = createObject(TestCategoryV, TestCategoryPostV, 'testCategories');
   public static read = readObject(TestCategoryV, 'testCategories');
   public static delete = deleteObject(TestCategoryV, 'testCategories');
   public static update = updateObject(TestCategoryV, TestCategoryPatchV, 'testCategories');
-
-  public static run = createObjectDetail(TaskV, TestInputs, 'testCategories', 'run');
 
   public static sort = (categories: TestCategoryType[]): TestCategoryType[] => {
     const compare = (a: TestCategoryType, b: TestCategoryType) => {
