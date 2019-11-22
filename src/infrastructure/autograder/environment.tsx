84a3356864448b940e80e80d701d174b0e5011ff
vayyala <vayyala08@gmail.com>
2019-11-22 18:04:44 -0500

    Add dump mode to environment fields. Update eject template types

--- src/infrastructure/autograder/environment.tsx
+++ src/infrastructure/autograder/environment.tsx
@@ -23,6 +23,7 @@ const EnvironmentV = t.intersection(
       compileText: t.string,
       isRunning: t.boolean,
       sourceFiles: t.array(t.number),
+      dumpMode: t.boolean,
     }),
     t.partial({}),
   ],
@@ -37,6 +38,7 @@ const EnvironmentVPost = t.intersection(
       assignment: t.number,
       dependencies: t.string,
       compileText: t.string,
+      dumpMode: t.boolean,
     }),
     t.partial({}),
   ],
@@ -50,6 +52,7 @@ const EnvironmentVPatch = t.intersection(
       language: t.union([t.string, t.null]),
       dependencies: t.string,
       compileText: t.string,
+      dumpMode: t.boolean,
     }),
   ],
   'EnvironmentPatch',
@@ -67,8 +70,9 @@ const BuildData = t.intersection([
 const TestTemplate = t.intersection([
   GenericObject,
   t.type({
+    name: t.string,
     code: t.string,
-    extension: t.string,
+    errorIfMissing: t.union([t.null, t.boolean]),
   }),
 ]);
 
