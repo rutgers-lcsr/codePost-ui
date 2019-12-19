ff03a0f6aa1a2b64cfd021be177d99c19585f5c3
vayyala <vayyala08@gmail.com>
2019-12-19 18:43:29 -0500

    add a build type, and docker download

--- src/components/admin/assignments/tests/edit/EnvironmentSpecs.tsx
+++ src/components/admin/assignments/tests/edit/EnvironmentSpecs.tsx
@@ -6,7 +6,7 @@
 import React, { useState, useEffect } from 'react';
 
 /* library imports */
-import { Modal, Button, Divider, Icon, Input, Select, Tooltip, Tag, Typography, Empty, message } from 'antd';
+import { Modal, Button, Divider, Icon, Radio, Select, Spin, Tooltip, Tag, Typography, Empty } from 'antd';
 
 /* codePost object imports */
 import { AssignmentPatchType, AssignmentType } from '../../../../../infrastructure/assignment';
@@ -16,9 +16,10 @@ import { EnvironmentType } from '../../../../../infrastructure/autograder/enviro
 import { CodeWindow } from './utils/CodeWindow';
 
 /* codePost util imports */
-import { languages, hasDependenciesSupport } from './utils/languageUtils';
+import { languages } from './utils/languageUtils';
 
 import { TestFileList } from './EnvironmentSpecs/TestFileList';
+import { Environment } from '../../../../../infrastructure/autograder/environment';
 
 import { SolutionFileType } from '../../../../../infrastructure/autograder/solutionFile';
 import { HelperFileType } from '../../../../../infrastructure/autograder/helperFile';
@@ -40,20 +41,22 @@ interface IProps {
   currentAssignment: AssignmentType;
   updateAssignment: (assignment: AssignmentPatchType) => Promise<void>;
   env: EnvironmentType | undefined;
-  buildEnv: (language: string, dependencies: string[]) => Promise<void>;
+  buildEnv: (language: string, dependencies: string[], buildType: string) => Promise<void>;
   updateCompileText: (compileText: string) => Promise<void>;
   helpers: SolutionFileType[] | HelperFileType[];
   solutions: SolutionFileType[] | HelperFileType[];
   addFile: (type: FILE_TYPE, name: string, code: string, path?: string) => Promise<void>;
   deleteFile: (type: FILE_TYPE, id: number) => Promise<void>;
   updateFile: (type: FILE_TYPE, id: number, newCode: string) => Promise<void>;
+  loading: boolean;
 }
 
 export const EnvironmentSpecs = (props: IProps) => {
   /******************************* State Variables ****************************/
   const [language, setLanguage] = useState<string | null>(props.env ? props.env.language : null);
+  const [buildType, setBuildType] = useState<string>(props.env ? props.env.buildType : 'default');
   const [dependencies, setDependencies] = useState<string[]>(props.env ? JSON.parse(props.env.dependencies) : []);
-  const [loading, setLoading] = useState(false);
+  const [buildIsLoading, setBuildIsLoading] = useState(false);
 
   /******************************* API / State Change Functions ****************************/
 
@@ -61,6 +64,7 @@ export const EnvironmentSpecs = (props: IProps) => {
     if (props.env) {
       setLanguage(props.env.language);
       setDependencies(JSON.parse(props.env.dependencies));
+      setBuildType(props.env.buildType);
     }
   }, [props.env]);
 
@@ -82,30 +86,51 @@ export const EnvironmentSpecs = (props: IProps) => {
   };
 
   const saveEnv = async () => {
-    setLoading(true);
-    try {
-      await props.buildEnv(language !== null ? language : '', dependencies);
-      setLoading(false);
-      message.success('Environment updated');
-    } catch (err) {
-      setLoading(false);
+    setBuildIsLoading(true);
+    if (buildType !== 'default' && props.env && buildType !== props.env.buildType) {
+      confirm({
+        title: `Are you sure you want to use a custom build?`,
+        content:
+          'When you use a custom build, the only default packages are those built into the operating system. Please make sure to install the required language packages for your langauge in the "Install Packages" field.',
+        async onOk() {
+          await props.buildEnv(language !== null ? language : '', dependencies, buildType);
+        },
+      });
+    } else {
+      await props.buildEnv(language !== null ? language : '', dependencies, buildType);
     }
+    setBuildIsLoading(false);
   };
 
   const saveCompileText = async (newText: string) => {
     await props.updateCompileText(newText);
   };
 
+  const downloadDockerfile = async () => {
+    if (props.env) {
+      const dockerfile = await Environment.dockerfile(props.env.id);
+      const a = document.createElement('a');
+      a.href = `data:text/plain;charset=utf-8,${dockerfile}`;
+      a.download = `${props.currentAssignment.name}-dockerfile`;
+      document.body.appendChild(a);
+      a.click();
+    }
+  };
+
   /******************************* State Change Functions ****************************/
   const onLanguageChange = (value: string) => {
     setLanguage(value);
+    setBuildType(value === 'other' ? 'alpine' : 'default');
+
     // if it's a new language, reset dependencies
     if (!props.env || value !== props.env.language) {
       setDependencies([]);
+      setBuildType(value === 'other' ? 'alpine' : 'default');
     }
     // if we return to old language, reset dependencies to props
     if (props.env && value === props.env.language) {
       setDependencies(JSON.parse(props.env.dependencies));
+      setBuildType(props.env.buildType);
     }
   };
 
@@ -113,8 +138,32 @@ export const EnvironmentSpecs = (props: IProps) => {
     setDependencies(newDependencies);
   };
 
+  const onBuildTypeChange = (e: any) => {
+    const newBuildType = e.target.value === 'default' ? 'default' : 'alpine';
+    setBuildType(newBuildType);
+    if (!props.env || newBuildType !== props.env.buildType) {
+      setDependencies([]);
+    }
+    if (props.env && newBuildType === props.env.buildType) {
+      setDependencies(JSON.parse(props.env.dependencies));
+    }
+  };
+
+  const onCustomBuildChange = (type: string) => {
+    setBuildType(type);
+  };
+
   /******************************* Return ****************************/
 
+  if (props.loading) {
+    return (
+      <div className="display-flex justify-content-center align-iterms-center">
+        <Spin style={{ marginTop: 15 }} />
+      </div>
+    );
+  }
+
+  //************ 1A. ENVIRONMENT -  SELECT LANGUAGE
   const selectLanguage = (
     // Disable selector if environment has a custom dockerfile defined
     <Select
@@ -132,11 +181,40 @@ export const EnvironmentSpecs = (props: IProps) => {
       })}
     </Select>
   );
+  const lookupValue = buildType === 'default' ? language : buildType;
+  const installText = (lookupValue && locale[lookupValue].installCmd) || '';
+  const envSpecText = lookupValue && locale[lookupValue].environment;
+  const languageIcon = (
+    <Tooltip title={envSpecText}>
+      <Icon type="database" theme="twoTone" twoToneColor={themeVars.theme.brandPrimary} />
+    </Tooltip>
+  );
 
-  // Fixme: refactor into util
-  const installText = (language && locale[language].installCmd) || '';
-  const envSpecText = language && locale[language].environment;
+  //************ 1B. ENVIRONMENT -  SELECT BUILD TYPE
+  const buildOptions = (
+    <Radio.Group onChange={onBuildTypeChange} value={buildType === 'default' ? 'default' : 'custom'}>
+      <Radio value={'default'} disabled={language === 'other'}>
+        Default (Recommended)
+      </Radio>
+      <Radio value={'custom'}>Custom</Radio>
+    </Radio.Group>
+  );
 
+  const customBuildSelect = buildType !== 'default' && (
+    <Select value={buildType} onChange={onCustomBuildChange} style={{ minWidth: 150 }}>
+      <Option key={'alpine'} value={'alpine'}>
+        Alpine-Linux
+      </Option>
+      <Option key={'ubuntu'} value={'ubuntu'}>
+        Ubuntu
+      </Option>
+      <Option key={'windows'} disabled={true} value={'windows'}>
+        Windows (coming soon)
+      </Option>
+    </Select>
+  );
+
+  //************ 1C. ENVIRONMENT -  SELECT DEPENDENCIES
   const selectDependencies = (
     // Disable selector if environment has a custom dockerfile defined
     <div style={{ marginLeft: 10 }}>
@@ -149,13 +227,12 @@ export const EnvironmentSpecs = (props: IProps) => {
         value={dependencies}
         placeholder={'Add package to environment'}
         onChange={onDependenciesChange}
-        disabled={
-          language === null || !hasDependenciesSupport(language) || (props.env && props.env.dockerfile.length > 0)
-        }
+        disabled={language === null || (props.env && props.env.dockerfile.length > 0)}
       />
     </div>
   );
 
+  //************ 1D. ENVIRONMENT - SHOW CUSTOM DOCKERFILE IF IT EXISTS
   const customDockerFile = props.env && props.env.dockerfile && (
     <span>Custom DockerFile:&nbsp;{props.env.dockerfile}</span>
   );
@@ -228,15 +305,20 @@ export const EnvironmentSpecs = (props: IProps) => {
       <div style={{ display: 'flex', justifyContent: 'space-between' }}>
         <Typography.Title level={3}>1. Define environment</Typography.Title>
         <div>
-          <Button type="primary" onClick={onSave} loading={loading}>
+          <Button type="primary" onClick={onSave} loading={buildIsLoading}>
             {props.env ? 'Update' : 'Create'}
           </Button>
+          {props.env && (
+            <Button style={{ marginLeft: 10 }} onClick={downloadDockerfile} disabled={buildIsLoading}>
+              Download
+            </Button>
+          )}
         </div>
       </div>
-      Language: {selectLanguage} &nbsp;
-      <Tooltip title={envSpecText}>
-        <Icon type="database" theme="twoTone" twoToneColor={themeVars.theme.brandPrimary} />
-      </Tooltip>
+      Language: {selectLanguage} &nbsp; {languageIcon}
+      <br />
+      <br />
+      <span style={{ lineHeight: '32px' }}>Build Type:</span> {buildOptions} {customBuildSelect}
       <br />
       <br />
       <div className="display-flex align-items-center">Install packages: {selectDependencies}</div>
