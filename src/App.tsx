eeb915fc3c21c581a6184f2c6aaa41d231699cf5
vayyala <vayyala08@gmail.com>
2020-01-08 11:44:57 -0800

    Revert "gauge speed by delaying intercom load. Suggested by this blog: https://www.daily.co/blog/webflow-sites-how-to-improve-your-google-pagespeed-score"
    This reverts commit 2a7cfb7dec7928390e8b373a7a06c0369e6dd52e.

--- src/App.tsx
+++ src/App.tsx
@@ -435,20 +435,18 @@ class App extends React.Component<{}, IState> {
         );
       }
 
-      setTimeout(function() {
-        if (isAdmin || isGrader) {
-          (window as any).Intercom('boot', {
-            app_id: 'kg4u5rp1',
-            email: user.email,
-            user_id: user.email,
-            custom_launcher_selector: '#IntercomDefaultWidget',
-            isAdmin: String(isAdmin),
-            isGrader: String(isGrader),
-          });
-        } else {
-          (window as any).Intercom('shutdown');
-        }
-      }, 10 * 1000);
+      if (isAdmin || isGrader) {
+        (window as any).Intercom('boot', {
+          app_id: 'kg4u5rp1',
+          email: user.email,
+          user_id: user.email,
+          custom_launcher_selector: '#IntercomDefaultWidget',
+          isAdmin: String(isAdmin),
+          isGrader: String(isGrader),
+        });
+      } else {
+        (window as any).Intercom('shutdown');
+      }
 
       const consoleProps = {
         user: this.state.user,
@@ -588,10 +586,10 @@ class App extends React.Component<{}, IState> {
         </div>
       );
     } else {
-      // (window as any).Intercom('boot', {
-      //   app_id: 'kg4u5rp1',
-      //   custom_launcher_selector: '#IntercomDefaultWidget',
-      // });
+      (window as any).Intercom('boot', {
+        app_id: 'kg4u5rp1',
+        custom_launcher_selector: '#IntercomDefaultWidget',
+      });
       return <div />;
     }
   }
