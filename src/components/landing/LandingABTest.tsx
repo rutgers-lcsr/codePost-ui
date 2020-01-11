e255d7f13fea9ecc58471731f459b890d3fd4096
vayyala <vayyala08@gmail.com>
2020-01-10 16:42:01 -0800

    i think the react-optimize package was unnecessarily adding too long to wait. Re-writing the code to fetch google optimize variants using: https://support.google.com/optimize/answer/9059383?hl=en and https://medium.com/adhawk-engineering/how-to-add-google-optimize-a-b-testing-to-your-react-app-in-10-lines-of-code-8310b58e51f4

--- src/components/landing/LandingABTest.tsx
+++ src/components/landing/LandingABTest.tsx
@@ -1,21 +1,60 @@
 import { Experiment, Variant } from 'react-optimize';
 
-import * as React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import LandingOld from './Landing.tsx';
 import LandingNew from './newlanding/Landing.tsx';
 
 const Landing = (props: any) => {
-  return (
-    <Experiment id="xsaR0R8XRW-wxSJMV-MfuQ">
-      <Variant id="0">
-        <LandingOld {...props} />
-      </Variant>
-      <Variant id="1">
-        <LandingNew {...props} />
-      </Variant>
-    </Experiment>
-  );
+  // Set up an A/B Test
+  // FIXME: Standardize this code to make a standard A/B test function
+  const [variant, setVariant] = useState(0);
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    const fetchvariant = async () => {
+      if ((window as any).dataLayer) {
+        const promise = (window as any).dataLayer.push({ event: 'optimize.activate' });
+        await promise;
+      }
+    };
+    fetchvariant();
+
+    let timesRun = 0; // we don't want our interval to stall indefinetely
+    const interval = setInterval(() => {
+      console.log('trying');
+      timesRun += 1;
+      if ((window as any).google_optimize !== undefined) {
+        const variant = (window as any).google_optimize.get('g9Q7V62ERN6WIYJcHTmRgw');
+        console.log(variant);
+        if (variant !== undefined) {
+          setVariant(variant);
+          setLoaded(true);
+          clearInterval(interval);
+        }
+      }
+      if (timesRun > 10) {
+        setLoaded(true);
+        clearInterval(interval);
+      }
+    }, 100);
+
+    return () => {
+      clearInterval(interval);
+    };
+    // Should implement useCallback()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  if (!loaded) {
+    return <div />;
+  }
+
+  if (variant === 1) {
+    return <LandingNew {...props} />;
+  }
+
+  return <LandingOld {...props} />;
 };
 
 export default Landing;
