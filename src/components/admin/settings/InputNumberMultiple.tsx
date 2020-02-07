8eac9f0a1670a11b38119f6a5d2ebfca29b5451c
Richard <richard@codepost.io>
2020-02-06 23:02:46 -0600

    InputNumberMultiple component

---
+++ src/components/admin/settings/InputNumberMultiple.tsx
@@ -0,0 +1,86 @@
+import * as React from 'react';
+
+import { Icon, Input, InputNumber } from 'antd';
+
+interface IInputNumberMultipleProps {
+  value: number[];
+  onChange: any;
+}
+
+class InputNumberMultiple extends React.Component<IInputNumberMultipleProps, {}> {
+  // public handleOnOffChange = (status: any) => {
+  //   if (status === 'On') {
+  //     this.triggerChange(0);
+  //   } else {
+  //     this.triggerChange(null);
+  //   }
+  // };
+
+  // public handleNumberChange = (value: any) => {
+  //   this.triggerChange(value);
+  // };
+
+  public addRow = () => {
+    const updated = [...this.props.value, 0];
+    this.props.onChange(updated);
+  };
+
+  public removeRow = () => {
+    const updated = this.props.value.slice(0, this.props.value.length - 1);
+    this.props.onChange(updated);
+  };
+
+  public changeValue = (value: number, index: number) => {
+    const updated = [...this.props.value];
+    updated[index] = value;
+    this.props.onChange(updated);
+  };
+
+  render() {
+    return (
+      <span>
+        {this.props.value.map((dayDeduction: number, day: number) => {
+          const dayString = `${day + 1}${day === this.props.value.length - 1 ? '+' : ''} day${
+            day + 1 === 1 ? '' : 's'
+          } late`;
+
+          const onChange = (changedValue: number | undefined) => {
+            this.changeValue(changedValue !== undefined ? changedValue : 0, day);
+          };
+
+          return (
+            <div
+              key={`day-${day + 1}`}
+              style={{ width: '171px', display: day === this.props.value.length - 1 ? 'inline-block' : 'block' }}
+            >
+              <span>
+                <Input.Group compact>
+                  <Input disabled={true} value={dayString} style={{ width: '110px' }} />
+                  <InputNumber min={0} value={dayDeduction} style={{ width: '60px' }} onChange={onChange} />
+                </Input.Group>
+              </span>
+            </div>
+          );
+        })}
+        {this.props.value.length !== 0 ? (
+          <span>
+            <Icon
+              type="minus-circle"
+              style={{ color: '#eb6f00', paddingLeft: '10px', cursor: 'pointer' }}
+              onClick={this.removeRow}
+            />
+          </span>
+        ) : null}
+        <span>
+          <Icon
+            type="plus-circle"
+            style={{ color: '#24be85', paddingLeft: '10px', cursor: 'pointer' }}
+            onClick={this.addRow}
+          />
+        </span>
+      </span>
+    );
+  }
+}
+
+export default InputNumberMultiple;
