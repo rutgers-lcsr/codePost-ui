6452cf68b8983f484df7243c028f683c95182ab1
Richard <richard@codepost.io>
2020-02-07 14:03:42 -0600

    ✏️

--- src/components/code-review/menu/SubmissionInfoMenu.tsx
+++ src/components/code-review/menu/SubmissionInfoMenu.tsx
@@ -65,7 +65,7 @@ const SubmissionInfo = (props: ISubmissionReadProps & ISubmissionInfoWriteProps)
   if (props.submission !== undefined) {
     if (props.submission) {
       if (props.assignment.uploadDueDate) {
-        const two_hours = 3.6e6 * 2; // ms
+        const two_hours = 3.6e6 * 2; // ms grace period
         const isLate =
           Date.parse(props.submission.dateUploaded) > Date.parse(props.assignment.uploadDueDate) + two_hours;
 
