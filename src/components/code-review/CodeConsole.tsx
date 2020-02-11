320bb6438abf53e86f4b4e5f6b3f4d61a1c8e47d
jamesaevans <james.alb.evans@gmail.com>
2020-02-11 09:02:12 -0800

    activate comment sharing for students

--- src/components/code-review/CodeConsole.tsx
+++ src/components/code-review/CodeConsole.tsx
@@ -592,11 +592,18 @@ class CodeConsole extends React.Component<ICodeConsoleProps, ICodeConsoleState> 
 
         files = CodeConsole.fileBouncer(files);
 
-        selectedFile = files.find((f: FileType) => {
-          return f.id === LOCAL_SETTINGS.mostRecentFile.getter();
-        });
         if (selectedFile === undefined && files.length > 0) {
-          selectedFile = files[0];
+          if (typeof queryValues.comment === 'string') {
+            const matchingFile = files.find((el) =>
+              el.comments.some((c) => c === parseInt(queryValues.comment as string)),
+            );
+            selectedFile = matchingFile || files[0];
+          } else {
+            selectedFile =
+              files.find((f: FileType) => {
+                return f.id === LOCAL_SETTINGS.mostRecentFile.getter();
+              }) || files[0];
+          }
         }
 
         // Read tests
@@ -1908,6 +1915,7 @@ Days late (after credit):  ${daysLateAfterCredit}
               hideAuthor={this.state.assignment.hideGradersFromStudents}
               additiveGrading={false}
               rubricCategories={this.state.rubricCategories}
+              scrollToCommentID={parseInt(queryString.parse(this.props.location.search).comment as string)}
             />
           );
 
