c32cd6b8862d8ef7153de9b3014189d9fa7c483f
vayyala <vayyala08@gmail.com>
2020-02-23 18:13:27 -0800

    refactor out the upload confirm content

--- src/components/admin/assignments/assignments/SubmissionUpload/UploadSubmissionBulkDialog.tsx
+++ src/components/admin/assignments/assignments/SubmissionUpload/UploadSubmissionBulkDialog.tsx
@@ -23,7 +23,7 @@ import { AssignmentType, CourseType, SubmissionType } from '../../../../../infra
 
 import UploadExternal from './UploadExternal';
 
-import { IntegrationButton, INTEGRATIONS } from '../../../../landing/Integrations';
+import { INTEGRATIONS } from '../../../../landing/Integrations';
 
 import { codePostFile, IProtoFileUpload, fileToProtoFileUpload, readUploadedFile } from './FileReader';
 
@@ -180,14 +180,9 @@ class UploadSubmissionBulkDialog extends React.Component<IProps, IState> {
     return newMap;
   };
 
-  public isEqual = (string1: string, string2: string) => {
-    // Case insensitive string compare
-    return string1.toLowerCase() === string2.toLowerCase();
-  };
-
   public isValidStudent = (student: string, students: string[]) => {
     return students.some((el) => {
-      return this.isEqual(el, student);
+      return isEqual(el, student);
     });
   };
 
@@ -200,21 +195,6 @@ class UploadSubmissionBulkDialog extends React.Component<IProps, IState> {
       this.readFiles();
     });
   };
-
-  public getSubforStudent = (student: string, protoSubmissions: IProtoSubmission[]) => {
-    for (const sub of protoSubmissions) {
-      if (
-        sub.students.some((el) => {
-          return this.isEqual(el, student);
-        })
-      ) {
-        return sub;
-      }
-    }
-
-    return undefined;
-  };
-
   /***************************************************************************************/
   /* Upload business logic
   /***************************************************************************************/
@@ -372,12 +352,12 @@ class UploadSubmissionBulkDialog extends React.Component<IProps, IState> {
     // remove the student from submission.students
     const toChange: SubmissionType[] = [];
     students.forEach((student) => {
-      const newSubmission = this.getSubforStudent(student, this.state.protoSubmissions);
+      const newSubmission = getSubforStudent(student, this.state.protoSubmissions);
 
       if (newSubmission !== undefined && newSubmission.isCollision) {
         const match = submissions.find((submission) => {
           return submission.students.some((el) => {
-            return this.isEqual(el, student);
+            return isEqual(el, student);
           });
         });
 
@@ -388,12 +368,12 @@ class UploadSubmissionBulkDialog extends React.Component<IProps, IState> {
 
           if (reMatch) {
             reMatch.students = reMatch.students.filter((el) => {
-              return !this.isEqual(el, student);
+              return !isEqual(el, student);
             });
           } else {
             const newSub = { ...match };
             newSub.students = newSub.students.filter((el) => {
-              return !this.isEqual(el, student);
+              return !isEqual(el, student);
             });
             toChange.push(newSub);
           }
@@ -732,201 +712,24 @@ class UploadSubmissionBulkDialog extends React.Component<IProps, IState> {
       case STATUS.FILE_ERROR:
       case STATUS.UPLOADED:
         // separate students into lists for presentation
-        const studentLists = {
-          impacted: {} as { [student: string]: IProtoSubmission },
-          missing: [] as string[],
-          uploaded: [] as string[],
-        };
-
-        const lowerCaseStudents = this.props.students.map((student) => {
-          return student.toLowerCase();
-        });
-
-        for (const student of lowerCaseStudents) {
-          const sub = this.getSubforStudent(student, this.state.protoSubmissions);
-          if (sub !== undefined) {
-            studentLists.impacted[student] = sub;
-          } else {
-            // does student have an existing submission?
-            if (this.state.studentMap[student] === STUDENT_STATUS.EXISTING) {
-              studentLists.uploaded.push(student);
-            } else {
-              studentLists.missing.push(student);
-            }
+        numToUpload = this.state.protoSubmissions.reduce((acc, sub) => {
+          // Upload if submission has no collisions or has collision and overwrite mode is on
+          if (!sub.isCollision || (sub.isCollision && this.state.overwriteMode)) {
+            return acc + 1;
           }
-        }
-
-        // columns for impacted table
-        const columns = [
-          {
-            title: 'Student',
-            dataIndex: 'student',
-            key: 'student',
-          },
-          {
-            title: 'Status',
-            dataIndex: 'status',
-            key: 'status',
-            align: 'center' as 'center' | 'left' | 'right',
-          },
-          {
-            title: 'Partners',
-            dataIndex: 'partners',
-            key: 'partners',
-          },
-          {
-            title: 'Files',
-            dataIndex: 'files',
-            key: 'files',
-          },
-        ];
-
-        // columns for non-impacted tables
-        const studentColumns = [
-          {
-            title: 'Student',
-            dataIndex: 'student',
-            key: 'student',
-          },
-        ];
-
-        // data for impacted table
-        let hasCollisions = false;
-        const dataSource = Object.keys(studentLists.impacted)
-          .sort((a, b) => {
-            const studentMap = this.state.studentMap;
-            return studentMap[a] - studentMap[b];
-          })
-          .map((el) => {
-            let status;
-            const sub = studentLists.impacted[el];
-
-            if (sub.isCollision) {
-              hasCollisions = true;
-              if (this.state.overwriteMode) {
-                numToUpload = numToUpload + 1;
-                status = (
-                  <Tag color="green" key={el}>
-                    Ok
-                  </Tag>
-                );
-              } else {
-                let tooltipText;
-                if (this.state.studentMap[el] === STUDENT_STATUS.EXISTING) {
-                  tooltipText = 'This student already has a submission uploaded for this assignment.';
-                } else {
-                  tooltipText = `One of this student's partners in the submission you
-                     uploaded aleady has a submission uploaded for this assignment.`;
-                }
-                status = (
-                  <CPTooltip title={tooltipText}>
-                    <Tag color="volcano" key={el}>
-                      CONFLICT
-                    </Tag>
-                  </CPTooltip>
-                );
-              }
-            } else {
-              numToUpload = numToUpload + 1;
-              status = (
-                <Tag color="green" key={el}>
-                  Ok
-                </Tag>
-              );
-            }
-
-            // tslint:disable
-            return {
-              key: el,
-              student: el,
-              status,
-              partners: sub ? sub.students.filter((student) => !this.isEqual(student, el)).join(', ') : '',
-              files: sub
-                ? sub.files
-                    .map((file) => {
-                      return file.name;
-                    })
-                    .join(', ')
-                : '',
-            };
-            // tslint:enable
-          });
-
-        // data for non-impacted tables
-        const withSubmissionsData = studentLists.uploaded.map((el) => {
-          return {
-            student: el,
-          };
-        });
-
-        const withoutSubmissionsData = studentLists.missing.map((el) => {
-          return {
-            student: el,
-          };
-        });
-
-        // for customizing instructions
-        const numSubmissions = Object.values(studentLists.impacted).length;
-        const numStudents = Object.keys(studentLists.impacted).length;
-
+          return acc;
+        }, 0);
         content = (
-          <div>
-            {this.state.errorPaths.length > 0 ? (
-              <div>
-                <Divider orientation="left" style={{ color: 'red' }}>
-                  Errors
-                </Divider>
-                <div>
-                  <div>
-                    The following files were rejected. Hit "start over" if you want to re-upload submissions.{' '}
-                    <CPTooltip
-                      title={tooltips.admin.assignments.uploadSubmissionFileTypes}
-                      infoIcon={true}
-                      iconStyle={{ paddingLeft: 5 }}
-                    />
-                  </div>
-                  <LogViewer text={this.state.errorPaths.join('\n')} />
-
-                  <br />
-                </div>
-              </div>
-            ) : null}
-            <Divider orientation="left">Instructions</Divider>
-            You are about to upload <Typography.Text strong>{numSubmissions}</Typography.Text> submission
-            {numSubmissions > 1 ? 's ' : ' '}
-            corresponding to <Typography.Text strong>{numStudents}</Typography.Text> student
-            {numStudents > 1 ? 's' : ''}
-            . You can view information about the submissions you are about to upload below. If you want to make changes,
-            just hit "Start over" to re-upload.
-            <br />
-            <br />
-            {hasCollisions ? (
-              <div>
-                <br />
-                <Tag color="volcano" key="collision-warning">
-                  CONFLICT
-                </Tag>{' '}
-                &nbsp; Existing submissions will be overwritten by this upload. Turn on Overwrite mode if you want to
-                upload these submissions. Otherwise, they will be excluded from your upload.
-                <br />
-                <br /> Overwrite mode: &nbsp;{' '}
-                <Switch onChange={this.toggleOverwriteMode} defaultChecked={this.state.overwriteMode} /> &nbsp;
-                <br />
-                <br />
-              </div>
-            ) : null}
-            <Table pagination={{ pageSize: 5 }} dataSource={dataSource} columns={columns} />
-            <Divider orientation="left">Students not uploaded</Divider>
-            <Collapse>
-              <Panel header="Students without submissions" key="1">
-                <Table pagination={{ pageSize: 5 }} dataSource={withoutSubmissionsData} columns={studentColumns} />
-              </Panel>
-              <Panel header="Students with submissions" key="2">
-                <Table pagination={{ pageSize: 5 }} dataSource={withSubmissionsData} columns={studentColumns} />
-              </Panel>
-            </Collapse>
-          </div>
+          <BulkUploadConfirm
+            students={this.props.students}
+            protoSubmissions={this.state.protoSubmissions}
+            studentMap={this.state.studentMap}
+            overwriteMode={this.state.overwriteMode}
+            toggleOverwriteMode={this.toggleOverwriteMode}
+            errorPaths={this.state.errorPaths}
+          />
         );
+
         break;
       case STATUS.READING:
         const readFiles = Object.keys(this.state.fileMap).reduce((acc, el) => {
@@ -1127,5 +930,230 @@ const UploadBulkFooter = (props: IUploadBulkFooterProps) => {
       {backButton} {forwardButton}
     </div>
   );
+};
+
+interface IBulkUploadConfirmProps {
+  students: string[];
+  protoSubmissions: IProtoSubmission[];
+  studentMap: { [student: string]: STUDENT_STATUS };
+  overwriteMode: boolean;
+  toggleOverwriteMode: () => void;
+  errorPaths: string[];
+}
+
+const BulkUploadConfirm = (props: IBulkUploadConfirmProps) => {
+  const studentLists = {
+    impacted: {} as { [student: string]: IProtoSubmission },
+    missing: [] as string[],
+    uploaded: [] as string[],
+  };
+
+  const lowerCaseStudents = props.students.map((student) => {
+    return student.toLowerCase();
+  });
+
+  for (const student of lowerCaseStudents) {
+    const sub = getSubforStudent(student, props.protoSubmissions);
+    if (sub !== undefined) {
+      studentLists.impacted[student] = sub;
+    } else {
+      // does student have an existing submission?
+      if (props.studentMap[student] === STUDENT_STATUS.EXISTING) {
+        studentLists.uploaded.push(student);
+      } else {
+        studentLists.missing.push(student);
+      }
+    }
+  }
+
+  // columns for impacted table
+  const columns = [
+    {
+      title: 'Student',
+      dataIndex: 'student',
+      key: 'student',
+    },
+    {
+      title: 'Status',
+      dataIndex: 'status',
+      key: 'status',
+      align: 'center' as 'center' | 'left' | 'right',
+    },
+    {
+      title: 'Partners',
+      dataIndex: 'partners',
+      key: 'partners',
+    },
+    {
+      title: 'Files',
+      dataIndex: 'files',
+      key: 'files',
+    },
+  ];
+
+  // columns for non-impacted tables
+  const studentColumns = [
+    {
+      title: 'Student',
+      dataIndex: 'student',
+      key: 'student',
+    },
+  ];
+
+  // data for impacted table
+  let hasCollisions = false;
+  const dataSource = Object.keys(studentLists.impacted)
+    .sort((a, b) => {
+      const studentMap = props.studentMap;
+      return studentMap[a] - studentMap[b];
+    })
+    .map((el) => {
+      let status;
+      const sub = studentLists.impacted[el];
+
+      if (sub.isCollision) {
+        hasCollisions = true;
+        if (props.overwriteMode) {
+          status = (
+            <Tag color="green" key={el}>
+              Ok
+            </Tag>
+          );
+        } else {
+          let tooltipText;
+          if (props.studentMap[el] === STUDENT_STATUS.EXISTING) {
+            tooltipText = 'This student already has a submission uploaded for this assignment.';
+          } else {
+            tooltipText = `One of this student's partners in the submission you
+               uploaded aleady has a submission uploaded for this assignment.`;
+          }
+          status = (
+            <CPTooltip title={tooltipText}>
+              <Tag color="volcano" key={el}>
+                CONFLICT
+              </Tag>
+            </CPTooltip>
+          );
+        }
+      } else {
+        status = (
+          <Tag color="green" key={el}>
+            Ok
+          </Tag>
+        );
+      }
+
+      // tslint:disable
+      return {
+        key: el,
+        student: el,
+        status,
+        partners: sub ? sub.students.filter((student) => !isEqual(student, el)).join(', ') : '',
+        files: sub
+          ? sub.files
+              .map((file) => {
+                return file.name;
+              })
+              .join(', ')
+          : '',
+      };
+      // tslint:enable
+    });
+
+  // data for non-impacted tables
+  const withSubmissionsData = studentLists.uploaded.map((el) => {
+    return {
+      student: el,
+    };
+  });
+
+  const withoutSubmissionsData = studentLists.missing.map((el) => {
+    return {
+      student: el,
+    };
+  });
+
+  // for customizing instructions
+  const numSubmissions = Object.values(studentLists.impacted).length;
+  const numStudents = Object.keys(studentLists.impacted).length;
+
+  return (
+    <div>
+      {props.errorPaths.length > 0 ? (
+        <div>
+          <Divider orientation="left" style={{ color: 'red' }}>
+            Errors
+          </Divider>
+          <div>
+            <div>
+              The following files were rejected. Hit "start over" if you want to re-upload submissions.{' '}
+              <CPTooltip
+                title={tooltips.admin.assignments.uploadSubmissionFileTypes}
+                infoIcon={true}
+                iconStyle={{ paddingLeft: 5 }}
+              />
+            </div>
+            <LogViewer text={props.errorPaths.join('\n')} />
+
+            <br />
+          </div>
+        </div>
+      ) : null}
+      <Divider orientation="left">Instructions</Divider>
+      You are about to upload <Typography.Text strong>{numSubmissions}</Typography.Text> submission
+      {numSubmissions > 1 ? 's ' : ' '}
+      corresponding to <Typography.Text strong>{numStudents}</Typography.Text> student
+      {numStudents > 1 ? 's' : ''}
+      . You can view information about the submissions you are about to upload below. If you want to make changes, just
+      hit "Start over" to re-upload.
+      <br />
+      <br />
+      {hasCollisions ? (
+        <div>
+          <br />
+          <Tag color="volcano" key="collision-warning">
+            CONFLICT
+          </Tag>{' '}
+          &nbsp; Existing submissions will be overwritten by this upload. Turn on Overwrite mode if you want to upload
+          these submissions. Otherwise, they will be excluded from your upload.
+          <br />
+          <br /> Overwrite mode: &nbsp;{' '}
+          <Switch onChange={props.toggleOverwriteMode} defaultChecked={props.overwriteMode} /> &nbsp;
+          <br />
+          <br />
+        </div>
+      ) : null}
+      <Table pagination={{ pageSize: 5 }} dataSource={dataSource} columns={columns} />
+      <Divider orientation="left">Students not uploaded</Divider>
+      <Collapse>
+        <Panel header="Students without submissions" key="1">
+          <Table pagination={{ pageSize: 5 }} dataSource={withoutSubmissionsData} columns={studentColumns} />
+        </Panel>
+        <Panel header="Students with submissions" key="2">
+          <Table pagination={{ pageSize: 5 }} dataSource={withSubmissionsData} columns={studentColumns} />
+        </Panel>
+      </Collapse>
+    </div>
+  );
+};
+
+// HELPERS
+const getSubforStudent = (student: string, protoSubmissions: IProtoSubmission[]) => {
+  for (const sub of protoSubmissions) {
+    if (
+      sub.students.some((el) => {
+        return isEqual(el, student);
+      })
+    ) {
+      return sub;
+    }
+  }
+
+  return undefined;
+};
+
+const isEqual = (string1: string, string2: string) => {
+  // Case insensitive string compare
+  return string1.toLowerCase() === string2.toLowerCase();
 };
 export default UploadSubmissionBulkDialog;
