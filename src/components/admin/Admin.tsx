e95af2eae2a9b5e56bb536f0c1e57894746e44ca
jamesaevans <james.alb.evans@gmail.com>
2020-04-24 17:29:41 -0700

    only show set password modal if password not defined and source query arg defined

--- src/components/admin/Admin.tsx
+++ src/components/admin/Admin.tsx
@@ -125,7 +125,7 @@ class Admin extends React.Component<IComponentProps, IAdminState> {
     // We show the CIP modal if the source is in the url, or if the user doesn't have credentials
     // The second check (credentials) is to prevent the flow of: CIP user goes to create course, navigates to splash page, and doesn't set password
     const showCIPModal =
-      Object.hasOwnProperty.bind(queryString.parse(this.props.location.search))('source') ||
+      Object.hasOwnProperty.bind(queryString.parse(this.props.location.search))('source') &&
       !this.props.user.hasCredentials;
     const showOnboarding =
       Object.hasOwnProperty.bind(queryString.parse(this.props.location.search))('onboarding') ||
